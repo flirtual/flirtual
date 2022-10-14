2f17d63e663963d1cff2c149baa9cea24e5bac99
ariesclark <me@ariesclark.com>
2022-10-14 14:50:26 -0600

    switch to using bulk endpoint for initial seeding
    Former-commit-id: 7061c19fcdbaa3c59c24d2d3ce9bf72f7aa5d49d

--- api/lib/flirtual/elasticsearch/elasticsearch.ex
+++ api/lib/flirtual/elasticsearch/elasticsearch.ex
@@ -2,12 +2,33 @@ defmodule Flirtual.Elasticsearch do
   use Elasticsearch.Cluster, otp_app: :flirtual
 
   def dev_seed do
-    Elasticsearch.delete!(Flirtual.Elasticsearch, "/users")
-    Elasticsearch.Index.create_from_file(Flirtual.Elasticsearch, "users", "priv/elasticsearch/users.json")
-    users = File.read!("priv/elasticsearch/out.json") |> Jason.decode!
+    Elasticsearch.delete(Flirtual.Elasticsearch, "/users")
 
-    for user <- users do
-      Elasticsearch.put!(Flirtual.Elasticsearch, "/users/_doc/#{Map.get(user, "id")}", Map.drop(user, [:id]))
+    IO.inspect(
+      Elasticsearch.Index.create_from_file(
+        Flirtual.Elasticsearch,
+        "users",
+        "priv/elasticsearch/users.json"
+      )
+    )
+
+    users =
+      File.read!("priv/elasticsearch/out.json")
+      |> Jason.decode!()
+      |> Enum.chunk_every(500)
+
+    for chunk <- users do
+      body =
+        (Enum.map(chunk, &[%{"create" => %{"_id" => &1["id"]}}, &1])
+         |> List.flatten()
+         |> Enum.map(&Jason.encode!(&1))
+         |> Enum.join("\n")) <> "\n"
+
+      Elasticsearch.post!(
+        Flirtual.Elasticsearch,
+        "/users/_bulk",
+        body
+      )
     end
   end
 
