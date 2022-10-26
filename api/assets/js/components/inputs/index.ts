b4845c09dd3a9ec6fabf0f92d009ca584d8e0421
ariesclark <me@ariesclark.com>
2022-10-26 04:13:02 -0600

    write autocomplete and switch component
    Former-commit-id: c698287406bc3117057b2faec858ed21188fda67

--- api/assets/js/components/inputs/index.ts
+++ api/assets/js/components/inputs/index.ts
@@ -1,5 +1,7 @@
+export * from "./autocomplete";
 export * from "./checkbox";
 export * from "./label";
+export * from "./switch";
 export * from "./text";
 
 export * as Input from "./";
---
+++ api/assets/js/components/inputs/switch.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from "react";
+import { twMerge } from "tailwind-merge";
+
+const SwitchInput: React.FC<React.ComponentProps<"input"> & { text: string }> = ({
+	text,
+	...props
+}) => (
+	<div className="flex relative items-center justify-center h-10 w-14">
+		<input
+			{...props}
+			type="radio"
+			className={twMerge(
+				"absolute h-full w-full bg-transparent focus:shadow-none focus:ring-transparent focus:ring-offset-0 focus:outline-none rounded-none border-none checked:bg-brand-gradient peer",
+				props.className
+			)}
+		/>
+		<label className={twMerge("absolute pointer-events-none", props.checked && "text-white")}>
+			{text}
+		</label>
+	</div>
+);
+
+export interface SwitchProps {
+	value: boolean;
+	name: string;
+	onChange: (value: boolean) => void;
+}
+
+export const Switch: React.FC<SwitchProps> = (props) => {
+	const { value, name } = props;
+
+	const onChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+		(event) => {
+			props.onChange.call(null, event.target.value === "yes");
+		},
+		[props.onChange]
+	);
+
+	return (
+		<div className="bg-brand-grey shadow-brand-1 focus-within:ring-brand-coral rounded-xl flex overflow-hidden focus-within:ring-2 focus-within:ring-offset-2">
+			<SwitchInput checked={value} name={name} text="Yes" value="yes" onChange={onChange} />
+			<SwitchInput checked={!value} name={name} text="No" value="no" onChange={onChange} />
+		</div>
+	);
+};
