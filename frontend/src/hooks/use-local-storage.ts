15ce03dc63eaf32993a7e29ae8bbb1da54f52ecf
Aries Clark <me@ariesclark.com>
2022-11-29 22:23:41 -0700

    fix local storage hook typing bug
    Former-commit-id: 6883c6e94ef23e26711929a64a3016f9a4c7a5c8

--- frontend/src/hooks/use-local-storage.ts
+++ frontend/src/hooks/use-local-storage.ts
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-export function useLocalStorage<T extends string>(key: string, initial: T) {
+export function useLocalStorage<T extends string>(key: string, initial: T): [T, React.Dispatch<T>] {
 	const [value, setValue] = useState(
-		(typeof localStorage !== "undefined" && localStorage.getItem(key)) ?? initial
+		((typeof localStorage !== "undefined" && localStorage.getItem(key)) ?? initial) as string
 	);
 
 	return [
-		value,
+		value as T,
 		(newValue: T) => {
 			localStorage.setItem(key, newValue);
 			setValue(newValue);
