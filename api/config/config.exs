bee20168f8e82beb8f0073bd361d62faab17ecb1
Aries Clark <me@ariesclark.com>
2023-03-08 16:14:16 -0700

    a
    Former-commit-id: f74dd7976f9f254395d40812c127231265cbe872

--- api/config/config.exs
+++ api/config/config.exs
@@ -14,7 +14,7 @@ config :flirtual,
   talkjs_app_id: "GZddugeg",
   talkjs_access_token: "***REMOVED***",
   frontend_origin: "http://127.0.0.1:3000",
-  elasticsearch_access_token: "***REMOVED***",
+  elasticsearch_access_token: "***REMOVED***"
 
 config :flirtual,
   ecto_repos: [Flirtual.Repo]
