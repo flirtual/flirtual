f698e8b07a8c301c4f041258f7a8ab82db199a09
Kyle Farwell <m@kfarwell.org>
2023-03-22 18:28:44 -0400

    shadowban users reported multiple times, update Reports page
    Co-authored-by: Aries <me@ariesclark.com>
    
    Former-commit-id: 46c617e747dd13f8ac96d7c115cb71379cd3b787

--- frontend/src/app/(authenticated)/(moderator)/layout.tsx
+++ frontend/src/app/(authenticated)/(moderator)/layout.tsx
@@ -5,8 +5,6 @@ export const dynamic = "force-dynamic";
 export default function ModeratorLayout({ children }: React.PropsWithChildren) {
 	return (
 		// @ts-expect-error: Server Component
-		<SessionProvider tags={["moderator"]}>
-			{children}
-		</SessionProvider>
+		<SessionProvider tags={["moderator"]}>{children}</SessionProvider>
 	);
 }
