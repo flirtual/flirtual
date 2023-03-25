d212ad045d89eb6c8285d7eec57e25bfa3acff8c
Aries Clark <me@ariesclark.com>
2023-03-25 04:42:03 -0600

    feat: Profile more info toggle + Linebreaks between tag categories
    Former-commit-id: d25eef0ae4e375c8e03e4690adf2b887cc4480fb

--- frontend/src/components/profile/pill/collection.tsx
+++ frontend/src/components/profile/pill/collection.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+import { Attribute } from "~/api/attributes";
 import { User } from "~/api/user";
 import { useAttributeList } from "~/hooks/use-attribute-list";
 import { useSession } from "~/hooks/use-session";
-import { filterBy, findBy } from "~/utilities";
+import { urls } from "~/urls";
+import { capitalize, groupBy } from "~/utilities";
 
 import { Pill } from "./pill";
 
@@ -17,83 +23,119 @@ function getPersonalityLabels({ profile: { openness, conscientiousness, agreeabl
 	];
 }
 
+interface PillAttributeListProps {
+	attributes?: Array<Attribute>;
+	user: User;
+	href?: string;
+}
+
+const PillAttributeList: React.FC<PillAttributeListProps> = ({ user, attributes, href }) => {
+	const [session] = useSession();
+	if (!session) return null;
+
+	const sessionAttributeIds = session.user.profile.attributes.map(({ id }) => id);
+
+	if (!attributes?.length) return null;
+
+	return (
+		<div className="flex w-full flex-wrap gap-2">
+			{attributes.map(({ id, name }) => (
+				<Pill
+					active={session.user.id !== user.id && sessionAttributeIds.includes(id)}
+					href={href}
+					key={id}
+				>
+					{name}
+				</Pill>
+			))}
+		</div>
+	);
+};
+
 export const PillCollection: React.FC<{ user: User }> = (props) => {
 	const [session] = useSession();
 	const { user } = props;
 
-	const allSexualities = useAttributeList("sexuality");
-	const allGames = useAttributeList("game");
-	const allInterests = useAttributeList("interest");
+	const [expanded, setExpanded] = useState(false);
+
+	const allAttributes = [
+		...useAttributeList("sexuality"),
+		...useAttributeList("game"),
+		...useAttributeList("interest"),
+		...useAttributeList("platform"),
+		...useAttributeList("language"),
+		...useAttributeList("kink")
+	];
 
 	if (!session) return null;
 
 	const sessionPersonalityLabels = getPersonalityLabels(session.user);
 	const personalityLabels = getPersonalityLabels(user);
 
-	const sexualities = filterBy(user.profile.attributes, "type", "sexuality");
+	const attributes = groupBy(
+		[...user.profile.attributes.map(({ id }) => id), ...user.profile.languages]
+			.map((attribute) => allAttributes.find(({ id }) => attribute === id))
+			.filter(Boolean),
+		({ type }) => type
+	);
 
 	return (
-		<div className="flex flex-wrap gap-4">
-			{user.profile.serious && <Pill>Open to serious dating</Pill>}
-			{sexualities.length !== 0 && (
-				<div className="flex w-full flex-wrap gap-2">
-					{sexualities.map(({ id }) => {
-						const attribute = findBy(allSexualities, "id", id);
-						if (!attribute) return null;
-
-						return (
+		<AnimatePresence>
+			<div className="flex flex-wrap gap-4">
+				<div className="flex w-full">
+					{user.profile.serious && (
+						<Pill href={urls.settings.matchmaking()}>Open to serious dating</Pill>
+					)}
+				</div>
+				<PillAttributeList
+					attributes={attributes.sexuality}
+					href={urls.settings.tags}
+					user={user}
+				/>
+				{personalityLabels.length !== 0 && (
+					<div className="flex w-full flex-wrap gap-2">
+						{personalityLabels.map((personalityLabel) => (
 							<Pill
-								active={!!findBy(session.user.profile.attributes, "id", attribute.id)}
-								key={attribute.id}
+								active={sessionPersonalityLabels.includes(personalityLabel)}
+								key={personalityLabel}
 							>
-								{attribute.name}
+								{personalityLabel}
 							</Pill>
-						);
-					})}
-				</div>
-			)}
-			{personalityLabels.length !== 0 && (
-				<div className="flex w-full flex-wrap gap-2">
-					{personalityLabels.map((personalityLabel) => (
-						<Pill
-							active={sessionPersonalityLabels.includes(personalityLabel)}
-							key={personalityLabel}
-						>
-							{personalityLabel}
-						</Pill>
-					))}
-				</div>
-			)}
-			<div className="flex w-full flex-wrap gap-2">
-				{filterBy(user.profile.attributes, "type", "interest").map(({ id }) => {
-					const attribute = findBy(allInterests, "id", id);
-					if (!attribute) return null;
-
-					return (
-						<Pill
-							active={!!findBy(session.user.profile.attributes, "id", attribute.id)}
-							key={attribute.id}
-						>
-							{attribute.name}
-						</Pill>
-					);
-				})}
+						))}
+					</div>
+				)}
+				<PillAttributeList attributes={attributes.interest} href={urls.settings.tags} user={user} />
+				<PillAttributeList attributes={attributes.game} href={urls.settings.tags} user={user} />
+				{user.profile.domsub && (
+					<div className="flex w-full flex-wrap gap-2">
+						<Pill href={urls.settings.matchmaking()}>{capitalize(user.profile.domsub)}</Pill>
+					</div>
+				)}
+				{expanded ? (
+					<>
+						{user.profile.monopoly && (
+							<div className="flex w-full flex-wrap gap-2">
+								<Pill href={urls.settings.matchmaking()}>{capitalize(user.profile.monopoly)}</Pill>
+							</div>
+						)}
+						<PillAttributeList attributes={attributes.kink} href={urls.settings.nsfw} user={user} />
+						<PillAttributeList
+							attributes={attributes.language}
+							href={urls.settings.tags}
+							user={user}
+						/>
+						<PillAttributeList
+							attributes={attributes.platform}
+							href={urls.settings.tags}
+							user={user}
+						/>
+					</>
+				) : (
+					<button type="button" onClick={() => setExpanded(true)}>
+						<EllipsisHorizontalIcon className="h-8 w-8" />
+					</button>
+				)}
 			</div>
-			<div className="flex w-full flex-wrap gap-2">
-				{filterBy(user.profile.attributes, "type", "game").map(({ id }) => {
-					const attribute = findBy(allGames, "id", id);
-					if (!attribute) return null;
-
-					return (
-						<Pill
-							active={!!findBy(session.user.profile.attributes, "id", attribute.id)}
-							key={attribute.id}
-						>
-							{attribute.name}
-						</Pill>
-					);
-				})}
-			</div>
-		</div>
+		</AnimatePresence>
 	);
 };
