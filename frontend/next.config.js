7bc5e272784ffc2f7bc1014546228f31c94b86d0
Kyle Farwell <m@kfarwell.org>
2023-03-28 18:16:33 -0400

    redirects
    Former-commit-id: 5f5a81898f544ab62084e116c6ceb6709ecce426

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,30 @@ const nextConfig = {
 		"@heroicons/react/24/solid": {
 			transform: "@heroicons/react/24/solid/{{member}}"
 		}
+	},
+	async redirects() {
+		return [
+			{
+				source: "/discord",
+				destination: "https://discord.gg/flirtual",
+				permanent: false
+			},
+			{
+				source: "/invite",
+				destination: "https://homie.zone/invite",
+				permanent: false
+			},
+			{
+				source: "/speeddate",
+				destination: "https://vrchat.com/home/world/wrld_f5844d7c-dc4d-4ee7-bf3f-63c8e6be5539",
+				permanent: false
+			},
+			{
+				source: "/club",
+				destination: "https://vrchat.com/home/world/wrld_d4f2cc35-83b9-41e9-9b3a-b861691c8df4",
+				permanent: false
+			}
+		];
 	}
 };
 
