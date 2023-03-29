6427613e1f0d9eddd56cbbf2c383c63112c8be9c
Aries Clark <me@ariesclark.com>
2023-03-28 22:59:22 -0600

    fix: less flashy activity indicator
    Former-commit-id: 176327b84c2483771e907d62beec285d8e3b9247

--- frontend/src/components/profile/activity-indicator.tsx
+++ frontend/src/components/profile/activity-indicator.tsx
@@ -1,7 +1,5 @@
-import { twMerge } from "tailwind-merge";
-
-const ONE_DAY_IN_MILLISECONDS = 8.64e7;
-const TWO_WEEKS_IN_MILLISECONDS = 1.21e9;
+const oneDayInMiliiseconds = 8.64e7;
+const twoWeeksInMillieseconds = 1.21e9;
 
 export interface ActivityIndicatorProps {
 	lastActiveAt: Date;
@@ -10,27 +8,12 @@ export interface ActivityIndicatorProps {
 export const ActivityIndicator: React.FC<ActivityIndicatorProps> = ({ lastActiveAt }) => {
 	const timeSince = Date.now() - lastActiveAt.getTime();
 
-	const color =
-		timeSince < TWO_WEEKS_IN_MILLISECONDS
-			? timeSince < ONE_DAY_IN_MILLISECONDS
-				? "bg-green-500"
-				: "bg-yellow-500"
-			: "bg-black-70";
-	const text =
-		timeSince < TWO_WEEKS_IN_MILLISECONDS
-			? timeSince < ONE_DAY_IN_MILLISECONDS
-				? "Active today"
-				: "Active recently"
-			: "Offline";
-
-	if (text.includes("Offline")) return null;
-
-	return (
+	return timeSince < twoWeeksInMillieseconds ? (
 		<div className="flex items-center gap-2">
-			<div className={twMerge("h-4 w-4 rounded-full", color)}>
-				<div className={twMerge("h-4 w-4 animate-ping rounded-full", color)} />
-			</div>
-			<span className="select-none font-montserrat font-semibold">{text}</span>
+			<div className="h-4 w-4 rounded-full bg-green-500" />
+			<span className="select-none font-montserrat font-semibold">
+				{timeSince < oneDayInMiliiseconds ? "Active today" : "Active recently"}
+			</span>
 		</div>
-	);
+	) : null;
 };
