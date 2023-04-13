2bdcd19293e8e7c60467878db45a5ae520d42382
Kyle Farwell <m@kfarwell.org>
2023-04-13 17:40:48 -0400

    update PWA start_url, register service worker
    Former-commit-id: 261aa8c44da90ff0b95fe08a6cc43de359e5e6cf

---
+++ frontend/public/sw.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+
+addEventListener("fetch", (fetchEvent) => {
+	const request = fetchEvent.request;
+	if (request.method !== "GET") {
+		return;
+	}
+	fetchEvent.respondWith(
+		(async function () {
+			const responseFromFetch = fetch(request);
+			fetchEvent.waitUntil(
+				(async function () {
+					const responseCopy = (await responseFromFetch).clone();
+					const myCache = await caches.open("files");
+					await myCache.put(request, responseCopy);
+				})()
+			);
+			if (request.headers.get("Accept").includes("text/html")) {
+				try {
+					return await responseFromFetch;
+				} catch (error) {
+					return caches.match(request);
+				}
+			} else {
+				const responseFromCache = await caches.match(request);
+				return responseFromCache || responseFromFetch;
+			}
+		})()
+	);
+});
