06b2718b7437b00c580bc4419c7993d34241c4c7
Kyle Farwell <m@kfarwell.org>
2023-04-17 16:31:06 -0400

    profiles accessible by ID URL
    Former-commit-id: 32a4738d7a547f052c54a8cdc83a211d27436964

--- frontend/src/app/(authenticated)/(email-confirmed)/(sole-model)/[username]/profile-user.ts
+++ frontend/src/app/(authenticated)/(email-confirmed)/(sole-model)/[username]/profile-user.ts
@@ -5,10 +5,18 @@ import { cache } from "react";
 import { api } from "~/api";
 import { thruServerCookies, withSession } from "~/server-utilities";
 import { urls } from "~/urls";
+import { isUuid } from "~/utilities";
 
 export const getProfileUser = cache(async (username: string) => {
+	const session = await withSession();
+
+	if (session.user.id === username || session.user.username === username) redirect(urls.user.me);
+
+	if (isUuid(username))
+		return await api.user.get(username, thruServerCookies()).catch(() => redirect(urls.default));
+
 	return username === "me"
-		? (await withSession()).user
+		? session.user
 		: await api.user
 				.getByUsername(username, thruServerCookies())
 				.catch(() => redirect(urls.default));
