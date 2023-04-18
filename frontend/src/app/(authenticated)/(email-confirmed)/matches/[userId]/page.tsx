8cd8c998e60c7f8520e25cfe7d3b6aece8a807a7
Aries <10256477+ariesclark@users.noreply.github.com>
2023-04-18 01:00:03 -0600

    feat: fix initial "no matches" flash
    Former-commit-id: b565661800b59b7a414ff69c55c36812c7893440

--- frontend/src/app/(authenticated)/(email-confirmed)/matches/[userId]/page.tsx
+++ frontend/src/app/(authenticated)/(email-confirmed)/matches/[userId]/page.tsx
@@ -1,3 +1,19 @@
-export default async function ConversationPage() {
-	return null;
+import { ConversationChatbox } from "~/hooks/use-talkjs";
+
+export interface ConversationPageProps {
+	params: { userId: string };
+}
+
+export default function ConversationPage({ params: { userId } }: ConversationPageProps) {
+	return (
+		<div className="flex h-full w-full flex-col items-center justify-center md:pl-8">
+			<ConversationChatbox
+				className="h-[calc(100vh_-_8rem)] w-full overflow-hidden shadow-brand-1 md:h-[32rem] md:rounded-xl"
+				userId={userId}
+				options={{
+					theme: "next-mobile"
+				}}
+			/>
+		</div>
+	);
 }
--- frontend/src/app/(authenticated)/(email-confirmed)/matches/layout.tsx
+++ frontend/src/app/(authenticated)/(email-confirmed)/matches/layout.tsx
@@ -1,38 +1,25 @@
-"use client";
-
-import { ChevronLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import Link from "next/link";
-import { useSelectedLayoutSegment } from "next/navigation";
-import useSWR from "swr";
-
 import { api } from "~/api";
 import { ButtonLink } from "~/components/button";
 import { Footer } from "~/components/layout/footer";
 import { Header } from "~/components/layout/header";
 import { MobileBarNavigation } from "~/components/layout/navigation/mobile-bar";
 import { ModelCard } from "~/components/model-card";
-import { useScreenBreakpoint } from "~/hooks/use-screen-breakpoint";
-import { useSession } from "~/hooks/use-session";
-import { ConversationChatbox, ConversationInbox } from "~/hooks/use-talkjs";
+import { thruServerCookies, withSession } from "~/server-utilities";
 import { urls } from "~/urls";
 
-export default function ConversationsLayout() {
-	const userId = useSelectedLayoutSegment();
-	const HeaderIcon = userId ? ChevronLeftIcon : XMarkIcon;
-	const [session] = useSession();
+import { ConversationList } from "./conversation-list";
+import { BackButton } from "./back-button";
 
-	const isDesktop = useScreenBreakpoint("md");
+export default async function ConversationsLayout({ children }: { children: React.ReactNode }) {
+	const session = await withSession();
 
-	const { data: conversations = [] } = useSWR("conversations", () => api.conversations.list());
+	const conversations = await api.conversations.list(thruServerCookies());
 	const conversationCount = conversations.length;
 
-	const { data: likes = { count: {}, items: [] } } = useSWR("likes", () =>
-		api.matchmaking.listMatches({
-			query: { unrequited: true }
-		})
-	);
-
-	if (!session) return null;
+	const likes = await api.matchmaking.listMatches({
+		...thruServerCookies(),
+		query: { unrequited: true }
+	});
 
 	return (
 		<div className="flex min-h-screen grow flex-col items-center overflow-x-hidden bg-cream font-nunito text-black-80 dark:bg-black-80 dark:text-white-20 sm:flex-col">
@@ -64,41 +51,12 @@ export default function ConversationsLayout() {
 				<div className="flex w-full max-w-screen-lg grow flex-col md:flex-row md:px-8">
 					<div className="flex h-full w-full shrink-0 grow-0 flex-col shadow-brand-1 md:w-96 md:rounded-t-xl md:bg-white-20 md:text-white-20 dark:md:bg-black-70">
 						<div className="flex h-16 w-full items-center justify-center bg-black-70 p-4 text-white-20 md:rounded-t-xl md:bg-brand-gradient">
-							<Link
-								className="absolute left-4 flex shrink-0 md:hidden"
-								href={userId ? urls.conversations.list : urls.browse()}
-							>
-								<HeaderIcon className="w-6" />
-							</Link>
+							<BackButton />
 							<span className="font-montserrat text-xl font-semibold">Messages</span>
 						</div>
-						{(isDesktop || !userId) && (
-							<ConversationInbox
-								className="h-[calc(100vh_-_8rem)] w-full md:h-screen"
-								options={{
-									showFeedHeader: false,
-									showMobileBackButton: false,
-									theme: "next-mobile",
-									feedFilter: {
-										custom: {
-											banned: "!exists"
-										}
-									}
-								}}
-							/>
-						)}
+						<ConversationList />
 					</div>
-					{userId && (
-						<div className="flex h-full w-full flex-col items-center justify-center md:pl-8">
-							<ConversationChatbox
-								className="h-[calc(100vh_-_8rem)] w-full overflow-hidden shadow-brand-1 md:h-[32rem] md:rounded-xl"
-								userId={userId}
-								options={{
-									theme: "next-mobile"
-								}}
-							/>
-						</div>
-					)}
+					{children}
 				</div>
 			)}
 			<Footer desktopOnly />
