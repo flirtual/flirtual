fa2c6dc1f9f60a13a5a6615aa5cd75cef8195742
Kyle Farwell <m@kfarwell.org>
2023-04-18 16:35:10 -0400

    add profile info to WidgetBot
    Former-commit-id: 516213d273ef253ee9e7ba3561842e3059e9a439

--- frontend/src/app/(public)/events/discord-embed.tsx
+++ frontend/src/app/(public)/events/discord-embed.tsx
@@ -3,13 +3,26 @@
 import WidgetBot from "@widgetbot/react-embed";
 import { useEffect, useState } from "react";
 
+import { displayName } from "~/api/user";
+import { useSession } from "~/hooks/use-session";
+import { urls } from "~/urls";
+
 export const DiscordEmbed: React.FC = () => {
 	const [clientSide, setClientSide] = useState(false);
 	useEffect(() => setClientSide(true), []);
 
+	const [session] = useSession();
+
 	if (!clientSide) return null;
 
 	return (
-		<WidgetBot channel="862116319700582440" height="600" server="455219574036496404" width="100%" />
+		<WidgetBot
+			avatar={session?.user ? urls.userAvatar(session?.user) : undefined}
+			channel="862116319700582440"
+			height="600"
+			server="455219574036496404"
+			username={session?.user ? displayName(session?.user) : undefined}
+			width="100%"
+		/>
 	);
 };
