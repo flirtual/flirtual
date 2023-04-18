11f9aa50dbe76e285d1c0490eeb9dd3218f7a762
Aries <10256477+ariesclark@users.noreply.github.com>
2023-04-17 23:16:32 -0600

    feat: detailed debugger page
    Former-commit-id: 946a67ce08dbcaafae66543b22bbd094ba5a2fae

--- frontend/src/app/(public)/debugger/page.tsx
+++ frontend/src/app/(public)/debugger/page.tsx
@@ -3,38 +3,86 @@ import { headers } from "next/headers";
 import { parse as parsePlatform } from "platform";
 
 import { api } from "~/api";
+import { displayName } from "~/api/user";
 import { InlineLink } from "~/components/inline-link";
 import { ModelCard } from "~/components/model-card";
+import { environment, gitCommitSha, gitCommitUrl } from "~/const-server";
+import { timeSince } from "~/date";
 import { withOptionalSession } from "~/server-utilities";
+import { urls } from "~/urls";
+import { capitalize } from "~/utilities";
 
 export default async function DebuggerPage() {
-	const session = await withOptionalSession();
-
-	console.log(process.env);
+	const session = await withOptionalSession().catch(() => null);
 
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 	const userAgent = headers().get("user-agent")!;
 	const { os, name, version, layout } = parsePlatform(userAgent);
 
 	return (
-		<div className="flex h-screen w-screen bg-brand-gradient sm:items-center sm:justify-center">
+		<div className="flex h-screen w-screen bg-cream font-nunito dark:bg-black-80 sm:items-center sm:justify-center">
 			<ModelCard className="h-full sm:h-fit" containerProps={{ className: "gap-4" }} title="Debug">
 				<div className="flex flex-col">
-					<span className="text-lg font-semibold">Browser</span>
-					<span className="font-mono text-sm">{`${name} ${version} (${layout})`}</span>
+					<span className="font-montserrat text-lg font-bold">Agent</span>
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">Browser: </span>
+						<span className="truncate font-mono">{`${name} ${version} (${layout})`}</span>
+					</div>
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">System: </span>
+						<span className="truncate font-mono text-sm">{os?.toString()}</span>
+					</div>
 				</div>
 				<div className="flex flex-col">
-					<span className="text-lg font-semibold">Operating System</span>
-					<span className="font-mono text-sm">{os?.toString()}</span>
+					<span className="font-montserrat text-lg font-bold">Session</span>
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">User profile: </span>
+						<InlineLink
+							className="truncate font-mono text-sm"
+							href={session ? urls.user.profile(session.user.username) : null}
+						>
+							{session ? `${displayName(session.user)} (${session.user.username})` : "Unavailable"}
+						</InlineLink>
+					</div>
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">User object: </span>
+						<InlineLink
+							className="truncate font-mono text-sm"
+							href={session ? api.newUrl(`users/${session.user.id}`) : null}
+						>
+							{session?.user.id ?? "Unavailable"}
+						</InlineLink>
+					</div>
+					{session?.sudoerId && (
+						<div className="flex justify-between gap-8 text-sm">
+							<span className="shrink-0">Sudoer object: </span>
+							<InlineLink
+								className="truncate font-mono text-sm"
+								href={api.newUrl(`users/${session.sudoerId}`)}
+							>
+								{session?.user.id}
+							</InlineLink>
+						</div>
+					)}
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">Age: </span>
+						<span className="truncate font-mono text-sm">
+							{session ? timeSince(new Date(session?.createdAt)) : ""}
+						</span>
+					</div>
 				</div>
 				<div className="flex flex-col">
-					<span className="text-lg font-semibold">User</span>
-					<InlineLink
-						className="font-mono text-sm"
-						href={session ? api.newUrl(`users/${session.user.id}`) : null}
-					>
-						{session?.user.id ?? "None"}
-					</InlineLink>
+					<span className="font-montserrat text-lg font-bold">Build</span>
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">Environment: </span>
+						<span className="truncate font-mono text-sm">{capitalize(environment)}</span>
+					</div>
+					<div className="flex justify-between gap-8 text-sm">
+						<span className="shrink-0">Commit: </span>
+						<InlineLink className="truncate font-mono text-sm" href={gitCommitUrl}>
+							{gitCommitSha || "Unavailable"}
+						</InlineLink>
+					</div>
 				</div>
 			</ModelCard>
 		</div>
--- frontend/src/date.ts
+++ frontend/src/date.ts
@@ -1,5 +1,11 @@
+import ms from "ms";
+
 const yearInMilliseconds = 3.154e10;
 
 export function yearsAgo(date: Date): number {
 	return Math.floor((Date.now() - date.getTime()) / yearInMilliseconds);
+}
+
+export function timeSince(date: Date): string {
+	return ms(date.getTime() / 1000, { long: true });
 }
