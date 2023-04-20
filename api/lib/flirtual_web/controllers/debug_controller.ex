af13ae3fb7e90271ec79c1725c774236e3afaee8
Aries <10256477+ariesclark@users.noreply.github.com>
2023-04-20 15:49:56 -0600

    fix: debug error handler
    Former-commit-id: 95ff5d39dde68008e9fc6107d1c68aa13eb70380

--- api/lib/flirtual_web/controllers/debug_controller.ex
+++ api/lib/flirtual_web/controllers/debug_controller.ex
@@ -65,10 +65,15 @@ defmodule FlirtualWeb.DebugController do
 
   def error(conn, %{"cipher" => cipher}) do
     with :ok <- Policy.can(conn, :read_error_cipher, conn.assigns[:session].user),
-         {:ok, reason} <- Crypto.decrypt(:error, cipher) do
-      conn |> json(reason |> Map.from_struct())
+         {:ok, reason} <- Crypto.decrypt(:error, cipher),
+         {:ok, reason} <-
+           reason
+           |> Map.from_struct()
+           |> Poison.encode() do
+      conn |> json(Poison.decode!(reason))
     else
       {:error, :invalid} -> {:error, {400, "Unknown cipher"}}
+      {:error, %Poison.EncodeError{}} -> {:error, {400, "Could not serialize error"}}
       reason -> reason
     end
   end
