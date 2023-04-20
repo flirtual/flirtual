6e0dfb7ddcc5980c81122ff6badd4278cd53508c
Aries <10256477+ariesclark@users.noreply.github.com>
2023-04-20 17:47:39 -0600

    feat: models shouldn't make request if they're not opened yet
    Former-commit-id: 553d18aad12a3b6ce5444b6feb2cd43b66a9bd63

--- frontend/src/components/modals/ban-profile.tsx
+++ frontend/src/components/modals/ban-profile.tsx
@@ -1,5 +1,5 @@
 import { ShieldExclamationIcon } from "@heroicons/react/24/solid";
-import { Dispatch, PropsWithChildren } from "react";
+import { Dispatch, FC, PropsWithChildren } from "react";
 
 import { api } from "~/api";
 import { displayName, User } from "~/api/user";
@@ -11,88 +11,99 @@ import { DrawerOrModal } from "../drawer-or-modal";
 import { Form, FormButton } from "../forms";
 import { InputLabel, InputSelect, InputTextArea } from "../inputs";
 
+export interface BanProfileModalFormProps {
+	user: User;
+	onVisibilityChange: Dispatch<boolean>;
+}
+
+export const BanProfileModalForm: FC<BanProfileModalFormProps> = ({ user, onVisibilityChange }) => {
+	const toasts = useToast();
+	const reasons = sortBy(useAttributeList("ban-reason"), ({ metadata }) => metadata.order);
+
+	return (
+		<Form
+			className="flex flex-col gap-8 rounded-3xl p-5 dark:text-white-20 sm:w-96"
+			requireChange={false}
+			fields={{
+				targetId: user.id,
+				reasonId: reasons[0]?.id,
+				message: reasons[0]?.metadata.details
+			}}
+			onSubmit={async ({ targetId, ...body }) => {
+				await api.user.suspend(targetId, { body });
+
+				toasts.add({
+					type: "success",
+					label: `Successfully banned profile!`,
+					children: <span className="text-sm">User: {user.id}</span>
+				});
+
+				onVisibilityChange(false);
+			}}
+		>
+			{({ FormField, fields: { message } }) => (
+				<>
+					<FormField className="flex flex-row items-center gap-4" name="targetId">
+						{() => (
+							<>
+								<ShieldExclamationIcon className="h-6 w-6" />
+								<span className="text-xl">Ban profile: {displayName(user)}</span>
+							</>
+						)}
+					</FormField>
+					<FormField name="reasonId">
+						{(field) => (
+							<>
+								<InputSelect
+									{...field.props}
+									options={reasons.map((attribute) => ({
+										key: attribute.id,
+										label: attribute.name
+									}))}
+									onChange={(reasonId) => {
+										field.props.onChange(reasonId);
+										message.props.onChange(
+											reasons.find((reason) => reason.id === reasonId)?.metadata.details || ""
+										);
+									}}
+								/>
+							</>
+						)}
+					</FormField>
+					<FormField name="message">
+						{(field) => (
+							<>
+								<InputLabel {...field.labelProps}>Message</InputLabel>
+								<InputTextArea
+									{...field.props}
+									placeholder="Write a custom ban reason for the user."
+									rows={6}
+								/>
+							</>
+						)}
+					</FormField>
+					<FormButton>Yonk</FormButton>
+				</>
+			)}
+		</Form>
+	);
+};
+
 type BanProfileModalProps = PropsWithChildren<{
 	user: User;
 	visible: boolean;
 	onVisibilityChange: Dispatch<boolean>;
 }>;
 
-export const BanProfileModal: React.FC<BanProfileModalProps> = ({
+export const BanProfileModal: FC<BanProfileModalProps> = ({
 	user,
 	children,
 	visible,
 	onVisibilityChange
 }) => {
-	const toasts = useToast();
-	const reasons = sortBy(useAttributeList("ban-reason"), ({ metadata }) => metadata.order);
-
 	return (
 		<DrawerOrModal visible={visible} onVisibilityChange={onVisibilityChange}>
-			<Form
-				className="flex flex-col gap-8 rounded-3xl p-5 dark:text-white-20 sm:w-96"
-				requireChange={false}
-				fields={{
-					targetId: user.id,
-					reasonId: reasons[0]?.id,
-					message: reasons[0]?.metadata.details
-				}}
-				onSubmit={async ({ targetId, ...body }) => {
-					await api.user.suspend(targetId, { body });
-
-					toasts.add({
-						type: "success",
-						label: `Successfully banned profile!`,
-						children: <span className="text-sm">User: {user.id}</span>
-					});
-
-					onVisibilityChange(false);
-				}}
-			>
-				{({ FormField, fields: { message } }) => (
-					<>
-						<FormField className="flex flex-row items-center gap-4" name="targetId">
-							{() => (
-								<>
-									<ShieldExclamationIcon className="h-6 w-6" />
-									<span className="text-xl">Ban profile: {displayName(user)}</span>
-								</>
-							)}
-						</FormField>
-						<FormField name="reasonId">
-							{(field) => (
-								<>
-									<InputSelect
-										{...field.props}
-										options={reasons.map((attribute) => ({
-											key: attribute.id,
-											label: attribute.name
-										}))}
-										onChange={(reasonId) => {
-											field.props.onChange(reasonId);
-											message.props.onChange(
-												reasons.find((reason) => reason.id === reasonId)?.metadata.details || ""
-											);
-										}}
-									/>
-								</>
-							)}
-						</FormField>
-						<FormField name="message">
-							{(field) => (
-								<>
-									<InputLabel {...field.labelProps}>Message</InputLabel>
-									<InputTextArea
-										{...field.props}
-										placeholder="Write a custom ban reason for the user."
-										rows={6}
-									/>
-								</>
-							)}
-						</FormField>
-						<FormButton>Yonk</FormButton>
-					</>
-				)}
-			</Form>
+			<BanProfileModalForm user={user} onVisibilityChange={onVisibilityChange} />
 			{children}
 		</DrawerOrModal>
 	);
--- frontend/src/components/modals/report-profile.tsx
+++ frontend/src/components/modals/report-profile.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, PropsWithChildren } from "react";
+import { Dispatch, FC, PropsWithChildren } from "react";
 import { ShieldExclamationIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 
@@ -10,6 +10,78 @@ import { DrawerOrModal } from "~/components/drawer-or-modal";
 import { Form, FormButton } from "~/components/forms";
 import { InputSelect, InputLabel, InputTextArea } from "~/components/inputs";
 import { useToast } from "~/hooks/use-toast";
+
+interface ReportProfileModelFormProps {
+	user: User;
+	onVisibilityChange: Dispatch<boolean>;
+}
+
+const ReportProfileModelForm: FC<ReportProfileModelFormProps> = ({ user, onVisibilityChange }) => {
+	const toasts = useToast();
+	const router = useRouter();
+	const reportReasons = useAttributeList("report-reason");
+
+	return (
+		<Form
+			className="flex flex-col gap-8 rounded-3xl p-5 dark:text-white-20 sm:w-96"
+			fields={{
+				targetId: user.id,
+				reasonId: reportReasons[0]?.id || null,
+				message: ""
+			}}
+			onSubmit={async ({ reasonId, targetId, message }) => {
+				if (!reasonId) return;
+				await api.report.create({ body: { reasonId, targetId, message } });
+
+				toasts.add({ type: "success", label: "Thank you for your report!" });
+				onVisibilityChange(false);
+
+				return router.refresh();
+			}}
+		>
+			{({ FormField }) => (
+				<>
+					<FormField className="flex flex-row items-center gap-4" name="targetId">
+						{() => (
+							<>
+								<ShieldExclamationIcon className="h-6 w-6" />
+								<span className="text-xl">Report profile: {displayName(user)}</span>
+							</>
+						)}
+					</FormField>
+					<FormField name="reasonId">
+						{(field) => (
+							<>
+								<InputSelect
+									{...field.props}
+									options={sortBy(reportReasons, ({ metadata }) => metadata.order).map(
+										(attribute) => ({
+											key: attribute.id,
+											label: attribute.name
+										})
+									)}
+								/>
+							</>
+						)}
+					</FormField>
+					<FormField name="message">
+						{(field) => (
+							<>
+								<InputLabel {...field.labelProps}>Message</InputLabel>
+								<InputTextArea
+									{...field.props}
+									placeholder="Tell us a little more about this incident. If you'd like us to reach out for more details or further evidence, please include your contact info."
+									rows={6}
+								/>
+							</>
+						)}
+					</FormField>
+					<FormButton>Report</FormButton>
+				</>
+			)}
+		</Form>
+	);
+};
 
 type ReportProfileModelProps = PropsWithChildren<{
 	user: User;
@@ -23,70 +95,9 @@ export const ReportProfileModel: React.FC<ReportProfileModelProps> = ({
 	visible,
 	onVisibilityChange
 }) => {
-	const toasts = useToast();
-	const router = useRouter();
-	const reportReasons = useAttributeList("report-reason");
-
 	return (
 		<DrawerOrModal visible={visible} onVisibilityChange={onVisibilityChange}>
-			<Form
-				className="flex flex-col gap-8 rounded-3xl p-5 dark:text-white-20 sm:w-96"
-				fields={{
-					targetId: user.id,
-					reasonId: reportReasons[0]?.id || null,
-					message: ""
-				}}
-				onSubmit={async ({ reasonId, targetId, message }) => {
-					if (!reasonId) return;
-					await api.report.create({ body: { reasonId, targetId, message } });
-
-					toasts.add({ type: "success", label: "Thank you for your report!" });
-					onVisibilityChange(false);
-
-					return router.refresh();
-				}}
-			>
-				{({ FormField }) => (
-					<>
-						<FormField className="flex flex-row items-center gap-4" name="targetId">
-							{() => (
-								<>
-									<ShieldExclamationIcon className="h-6 w-6" />
-									<span className="text-xl">Report profile: {displayName(user)}</span>
-								</>
-							)}
-						</FormField>
-						<FormField name="reasonId">
-							{(field) => (
-								<>
-									<InputSelect
-										{...field.props}
-										options={sortBy(reportReasons, ({ metadata }) => metadata.order).map(
-											(attribute) => ({
-												key: attribute.id,
-												label: attribute.name
-											})
-										)}
-									/>
-								</>
-							)}
-						</FormField>
-						<FormField name="message">
-							{(field) => (
-								<>
-									<InputLabel {...field.labelProps}>Message</InputLabel>
-									<InputTextArea
-										{...field.props}
-										placeholder="Tell us a little more about this incident. If you'd like us to reach out for more details or further evidence, please include your contact info."
-										rows={6}
-									/>
-								</>
-							)}
-						</FormField>
-						<FormButton>Report</FormButton>
-					</>
-				)}
-			</Form>
+			<ReportProfileModelForm user={user} onVisibilityChange={onVisibilityChange} />
 			{children}
 		</DrawerOrModal>
 	);
