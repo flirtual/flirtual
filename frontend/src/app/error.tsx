0031251a947704bcd680ea2abf6d5f5862ad7eb9
Kyle Farwell <m@kfarwell.org>
2023-04-25 18:53:11 -0400

    update error page copy
    Former-commit-id: dc4423536cbe71fc0d27cf038d88b197f2f8bc62

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -17,36 +17,40 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
 				<div className="flex flex-col gap-8">
 					<FlirtualLogo className="w-64" />
 					<h1 className="font-montserrat text-2xl font-semibold sm:text-3xl">
-						We&apos;re having some
-						<br className="hidden sm:block" /> troubles right now.
+						Something went wrong :(
 					</h1>
 					<div className="flex flex-col font-nunito">
 						<p>
-							We&apos;re figuring things out, but we&apos;ll be back soon!
-							<br className="hidden sm:block" /> Thank you for your patience!
+							Sorry about that! There was an error processing your request.
+							<br className="hidden sm:block" /> If this issue persists, please{" "}
+							<InlineLink className="text-white-10 underline" href={urls.resources.contact}>
+								contact us
+							</InlineLink>{" "}
+							or check back later.
+							<br className="hidden sm:block" /> Thank you for your patience &lt;3
 						</p>
 					</div>
 				</div>
 				<div className="flex gap-2">
 					<Button className="w-fit" kind="secondary" size="sm" onClick={reset}>
-						Reload
+						Try again
 					</Button>
 					<Button className="w-fit" kind="secondary" size="sm" onClick={() => router.back()}>
-						Back
+						Go back
 					</Button>
 				</div>
 				<div className="flex flex-col">
 					<div>
+						<InlineLink highlight={false} href={urls.resources.networkStatus}>
+							Network Status
+						</InlineLink>
+						{" • "}
 						<InlineLink highlight={false} href={urls.socials.discord}>
 							Discord
 						</InlineLink>
 						{" • "}
 						<InlineLink highlight={false} href={urls.socials.twitter}>
 							Twitter
-						</InlineLink>
-						{" • "}
-						<InlineLink highlight={false} href={urls.resources.networkStatus}>
-							Network Status
 						</InlineLink>
 					</div>
 					<footer>
