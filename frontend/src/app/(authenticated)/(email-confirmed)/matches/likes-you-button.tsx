d23cca36d5f2bef1becde4fb5329e37c620b9da6
Kyle Farwell <m@kfarwell.org>
2023-04-26 04:47:10 -0400

    include "See who likes you" button on "No matches" page (closes app-230)
    Former-commit-id: 7da97785da86407879d4f2f161643dd81082fe94

---
+++ frontend/src/app/(authenticated)/(email-confirmed)/matches/likes-you-button.tsx
@@ -0,0 +1,40 @@
+import { ButtonLink } from "~/components/button";
+import { HeartIcon } from "~/components/icons/gradient/heart";
+import { PeaceIcon } from "~/components/icons/gradient/peace";
+import { thruServerCookies } from "~/server-utilities";
+import { api } from "~/api";
+import { urls } from "~/urls";
+import { User } from "~/api/user";
+
+export async function LikesYouButton(user: User) {
+	const likes = await api.matchmaking.listMatches({
+		...thruServerCookies(),
+		query: {
+			unrequited: true
+		}
+	});
+
+	return (
+		<ButtonLink
+			className="w-full"
+			href={user.subscription?.active ? urls.likes : urls.subscription}
+			size="sm"
+		>
+			See who likes you{" "}
+			<span className="whitespace-nowrap">
+				{likes.count.love && (
+					<>
+						({likes.count.love > 99 ? "99+" : likes.count.love}
+						<HeartIcon className="inline h-4" gradient={false} />)
+					</>
+				)}{" "}
+				{likes.count.friend && (
+					<>
+						({likes.count.friend > 99 ? "99+" : likes.count.friend}
+						<PeaceIcon className="inline h-4" gradient={false} />)
+					</>
+				)}
+			</span>
+		</ButtonLink>
+	);
+}
