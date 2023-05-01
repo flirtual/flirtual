6aa1c4124916beb8610c9d94a3f23f40ef53a2c4
Aries <10256477+ariesclark@users.noreply.github.com>
2023-04-30 19:18:20 -0600

    feat: actually fix issue
    Former-commit-id: 7cd44e5b575684f3031f20d25be4d2de780ad90b

--- frontend/src/app/(authenticated)/(email-confirmed)/matches/page.tsx
+++ frontend/src/app/(authenticated)/(email-confirmed)/matches/page.tsx
@@ -1,8 +1,5 @@
 import { ConversationAside } from "./aside";
-import { withConversations } from "./data.server";
 
 export default async function ConversationListPage() {
-	const initialConversations = await withConversations();
-
-	return <ConversationAside initialConversations={initialConversations} />;
+	return <ConversationAside />;
 }
