2675633b0900dc5fdf945461ee53ef3ebe231266
Aries <10256477+ariesclark@users.noreply.github.com>
2023-04-30 18:47:44 -0600

    fix: Property 'initialConversations' does not exist on type 'IntrinsicAttributes & ConversationAsideProps'
    Former-commit-id: 5f7f2cca3cea02d6b6623c3f578b72210ecd1b68

--- frontend/src/app/(authenticated)/(email-confirmed)/matches/[userId]/page.tsx
+++ frontend/src/app/(authenticated)/(email-confirmed)/matches/[userId]/page.tsx
@@ -1,7 +1,6 @@
 import { ConversationChatbox } from "~/hooks/use-talkjs";
 
 import { ConversationAside } from "../aside";
-import { withConversations } from "../data.server";
 
 export interface ConversationPageProps {
 	params: {
@@ -14,11 +13,9 @@ export default async function ConversationPage(props: ConversationPageProps) {
 		params: { userId }
 	} = props;
 
-	const initialConversations = await withConversations();
-
 	return (
 		<>
-			<ConversationAside activeConversation={userId} initialConversations={initialConversations} />
+			<ConversationAside activeConversation={userId} />
 			<div className="flex h-full w-full flex-col items-center justify-center md:pl-8">
 				<ConversationChatbox
 					className="h-[calc(100vh-8rem)] w-full overflow-hidden shadow-brand-1 md:h-[32rem] md:rounded-xl"
