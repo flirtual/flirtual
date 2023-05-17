01d1d708f32c1555385c67e497fc2009fec9792e
Kyle Farwell <m@kfarwell.org>
2023-05-17 12:18:55 -0400

    update loading indicator colours
    Former-commit-id: 6a8f658d5a1bb0b430335e19e6c600f6b681f28d

--- frontend/src/components/loading-indicator-screen.tsx
+++ frontend/src/components/loading-indicator-screen.tsx
@@ -9,9 +9,9 @@ export const LoadingIndicator: React.FC<IconComponentProps> = (props) => {
 		<svg {...props} viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
 			<defs>
 				<linearGradient id={id} x1="8.042%" x2="65.682%" y1="0%" y2="23.865%">
-					<stop offset="0%" stopColor="#fff" stopOpacity="0" />
-					<stop offset="63.146%" stopColor="#fff" stopOpacity=".631" />
-					<stop offset="100%" stopColor="#fff" />
+					<stop offset="0%" stopColor="#e9658b" stopOpacity="0" />
+					<stop offset="63.146%" stopColor="#e9658b" stopOpacity=".631" />
+					<stop offset="100%" stopColor="#e9658b" />
 				</linearGradient>
 			</defs>
 			<g fill="none" fillRule="evenodd">
@@ -26,7 +26,7 @@ export const LoadingIndicator: React.FC<IconComponentProps> = (props) => {
 							type="rotate"
 						/>
 					</path>
-					<circle cx="36" cy="18" fill="#fff" r="1">
+					<circle cx="36" cy="18" fill="#e9658b" r="1">
 						<animateTransform
 							attributeName="transform"
 							dur="0.9s"
@@ -43,7 +43,7 @@ export const LoadingIndicator: React.FC<IconComponentProps> = (props) => {
 };
 
 export const LoadingIndicatorScreen: React.FC = () => (
-	<div className="fixed left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-black-80 p-4 md:p-16">
-		<LoadingIndicator className="w-16 grow-0 text-white-10" />
+	<div className="fixed left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-cream p-4 md:p-16">
+		<LoadingIndicator className="w-16 grow-0 text-pink" />
 	</div>
 );
