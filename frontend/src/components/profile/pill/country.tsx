a7d87315c6cefd08008879595000d8a0bfdacc87
Aries <10256477+ariesclark@users.noreply.github.com>
2023-05-20 21:38:27 -0600

    fix: country pill background opacity
    Former-commit-id: 88930f32b65f7cb7123d34a90eacf098dee3fe57

--- frontend/src/components/profile/pill/country.tsx
+++ frontend/src/components/profile/pill/country.tsx
@@ -8,20 +8,21 @@ import { Pill } from "./pill";
 export interface CountryPillProps {
 	code: string;
 	flagOnly?: boolean;
+	className?: string;
 }
 
-export async function CountryPill({ code, flagOnly = false }: CountryPillProps) {
+export async function CountryPill({ code, flagOnly = false, className }: CountryPillProps) {
 	const country = await withAttribute("country", code);
 	if (!country) return null;
 
 	return (
 		<Pill
-			className={twMerge("shrink-0", flagOnly && "overflow-hidden p-0")}
+			className={twMerge("shrink-0", flagOnly && "overflow-hidden p-0", className)}
 			hocusable={false}
 			small={true}
 		>
 			<img
-				className={twMerge("h-8 shrink-0", flagOnly ? "" : "-ml-4 rounded-l-xl")}
+				className={twMerge("aspect-[4/3] h-8 w-max shrink-0", flagOnly ? "" : "-ml-4 rounded-l-xl")}
 				src={country.metadata.flagUrl}
 			/>
 			{!flagOnly && <span>{country.name}</span>}
