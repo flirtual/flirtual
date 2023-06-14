0d975a4725d03c8d044fe38200f8c97df3e3ad94
Aries <10256477+ariesclark@users.noreply.github.com>
2023-06-14 12:55:52 -0600

    feat: initial capacitor android scaffolding

---
+++ apps/frontend/android/app/src/main/java/al/flirtu/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package al.flirtu.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/frontend/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	appId: "al.flirtu.mobile",
+	appName: "Flirtual",
+	webDir: "public",
+	server: {
+		androidScheme: "https"
+	}
+};
+
+export default config;
