935ea10dcaca503e74b1d3816f01efec1da912b8
Aries <10256477+ariesclark@users.noreply.github.com>
2023-06-19 21:30:10 -0600

    feat: pretty badge

--- apps/frontend/src/components/profile/verification-badge.tsx
+++ apps/frontend/src/components/profile/verification-badge.tsx
@@ -1,12 +1,15 @@
 import { CheckBadgeIcon } from "@heroicons/react/24/solid";
 
-import { Tooltip } from "../tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../tooltip";
 
 export const ProfileVerificationBadge: React.FC = () => (
-	<Tooltip value="Age verified">
-		<div className="relative h-6 w-6">
-			<div className="absolute left-1/4 top-1/4 h-3 w-3 bg-white-20" />
-			<CheckBadgeIcon className="absolute h-full w-full fill-theme-1" />
-		</div>
+	<Tooltip>
+		<TooltipTrigger asChild>
+			<div className="relative h-6 w-6">
+				<div className="absolute left-1/4 top-1/4 h-3 w-3 bg-white-20" />
+				<CheckBadgeIcon className="absolute h-full w-full fill-theme-2" />
+			</div>
+		</TooltipTrigger>
+		<TooltipContent>Age verified</TooltipContent>
 	</Tooltip>
 );
