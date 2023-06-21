02a3958836d28fe9ac12a99cf0e4cf45863e5cc8
Kyle Farwell <m@kfarwell.org>
2023-06-21 12:07:31 -0400

    fix session build?

--- apps/api/lib/flirtual_web/session.ex
+++ apps/api/lib/flirtual_web/session.ex
@@ -1,20 +1,14 @@
 defmodule FlirtualWeb.Session do
-  def init(opts) do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
     runtime_opts =
-      opts
-      |> Keyword.merge(
-        store: :cookie,
-        same_site: "Lax",
-        max_age: 3_888_000,
-        key: "session"
-      )
+      [store: :cookie, same_site: "Lax", max_age: 3_888_000, key: "session"]
       |> Keyword.put(:domain, Application.fetch_env!(:flirtual, :root_origin) |> Map.get(:host))
       |> Keyword.put(:signing_salt, Application.fetch_env!(:flirtual, :session_signing_salt))
 
-    Plug.Session.init(runtime_opts)
-  end
-
-  def call(conn, opts) do
-    Plug.Session.call(conn, opts)
+    Plug.Session.call(conn, Plug.Session.init(runtime_opts))
   end
 end
