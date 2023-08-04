29bb23644dd264078d5924b2ce00cc0d590c2332
Aries <10256477+ariesclark@users.noreply.github.com>
2023-08-03 18:09:53 -0600

    fix: useShare improvements (closes #132)

--- apps/frontend/src/hooks/use-share.tsx
+++ apps/frontend/src/hooks/use-share.tsx
@@ -1,12 +1,25 @@
 import { Share, ShareOptions } from "@capacitor/share";
 import { useCallback } from "react";
+import { Clipboard } from "@capacitor/clipboard";
+
+import { noop } from "~/utilities";
 
 import { useToast } from "./use-toast";
 
 export function useShare() {
-	const { addError } = useToast();
+	const { add: addToast } = useToast();
+
 	return useCallback(
-		(options: ShareOptions) => Share.share(options).catch(addError),
-		[addError]
+		async (options: ShareOptions) => {
+			await Clipboard.write(
+				options.url ? { url: options.url } : { string: options.text }
+			);
+
+			if (!(await Share.canShare()).value)
+				return addToast("Copied to clipboard");
+
+			await Share.share(options).catch(noop);
+		},
+		[addToast]
 	);
 }
