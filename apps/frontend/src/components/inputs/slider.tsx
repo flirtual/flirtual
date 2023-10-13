890794adfa9e6bb9a4b2dc29da4846ed6a29c9e3
Kyle Farwell <m@kfarwell.org>
2023-10-13 15:08:22 -0400

    grey out disabled sliders

--- apps/frontend/src/components/inputs/slider.tsx
+++ apps/frontend/src/components/inputs/slider.tsx
@@ -17,11 +17,11 @@ export const Slider = React.forwardRef<
 		{...props}
 	>
 		<SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-black-50 shadow-brand-1">
-			<SliderPrimitive.Range className="absolute h-full bg-brand-gradient" />
+			<SliderPrimitive.Range className="absolute h-full bg-brand-gradient data-[disabled]:bg-black-20 data-[disabled]:bg-none" />
 		</SliderPrimitive.Track>
 		{(props.value || props.defaultValue)?.map((_, index) => (
 			<SliderPrimitive.Thumb
-				className="block h-5 w-5 rounded-full border-2 border-none bg-brand-gradient shadow-brand-1 transition-colors focus:outline-none focus:ring-2 focus:ring-theme-1 focus:ring-offset-2 focus:ring-offset-white-20 disabled:pointer-events-none disabled:bg-black-40 disabled:opacity-50 dark:ring-offset-black-50"
+				className="block h-5 w-5 rounded-full border-2 border-none bg-brand-gradient shadow-brand-1 transition-colors focus:outline-none focus:ring-2 focus:ring-theme-1 focus:ring-offset-2 focus:ring-offset-white-20 disabled:pointer-events-none disabled:bg-black-40 disabled:opacity-50 data-[disabled]:bg-black-20 data-[disabled]:bg-none dark:ring-offset-black-50"
 				key={index}
 			/>
 		))}
