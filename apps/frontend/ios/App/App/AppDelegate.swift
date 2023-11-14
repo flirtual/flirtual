e11eb8d5a341129ca970657a3c0c1b54f52b8a29
Kyle Farwell <m@kfarwell.org>
2023-11-14 12:26:28 -0500

    clear badge when iOS app is opened

--- apps/frontend/ios/App/App/AppDelegate.swift
+++ apps/frontend/ios/App/App/AppDelegate.swift
@@ -27,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        application.applicationIconBadgeNumber = 0
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -53,5 +54,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
       NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
     }
+
 
 }
