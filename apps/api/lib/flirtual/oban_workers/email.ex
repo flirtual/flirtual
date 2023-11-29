d678b7a44fd76a980c8153384454a655b65c8888
Kyle Farwell <m@kfarwell.org>
2023-11-29 05:03:32 -0500

    email worker rate limiting

--- apps/api/lib/flirtual/oban_workers/email.ex
+++ apps/api/lib/flirtual/oban_workers/email.ex
@@ -13,17 +13,27 @@ defmodule Flirtual.ObanWorkers.Email do
             "body_html" => body_html
           } = args
       }) do
-    user = User.get(user_id)
-    from = Map.get(args, "from", "noreply@flirtu.al")
-    action_url = Map.get(args, "action_url", nil)
+    case ExRated.check_rate(
+           "email",
+           1000,
+           Application.get_env(:flirtual, Flirtual.ObanWorkers)[:email_rate_limit]
+         ) do
+      {:ok, _} ->
+        user = User.get(user_id)
+        from = Map.get(args, "from", "noreply@flirtu.al")
+        action_url = Map.get(args, "action_url", nil)
 
-    Mailer.send(
-      user,
-      from: from,
-      subject: subject,
-      action_url: action_url,
-      body_text: body_text,
-      body_html: body_html
-    )
+        Mailer.send(
+          user,
+          from: from,
+          subject: subject,
+          action_url: action_url,
+          body_text: body_text,
+          body_html: body_html
+        )
+
+      {:error, _} ->
+        {:snooze, 1}
+    end
   end
 end
