cfba555dc2c894b63ed8058fb01f32c01e92d7b5
Kyle Farwell <m@kfarwell.org>
2023-11-29 04:38:45 -0500

    fix: optional email args

--- apps/api/lib/flirtual/oban_workers/email.ex
+++ apps/api/lib/flirtual/oban_workers/email.ex
@@ -5,18 +5,21 @@ defmodule Flirtual.ObanWorkers.Email do
 
   @impl Oban.Worker
   def perform(%Oban.Job{
-        args: %{
-          "user_id" => user_id,
-          "subject" => subject,
-          "action_url" => action_url,
-          "body_text" => body_text,
-          "body_html" => body_html
-        }
+        args:
+          %{
+            "user_id" => user_id,
+            "subject" => subject,
+            "body_text" => body_text,
+            "body_html" => body_html
+          } = args
       }) do
     user = User.get(user_id)
+    from = Map.get(args, "from", nil)
+    action_url = Map.get(args, "action_url", nil)
 
     Mailer.send(
       user,
+      from: from,
       subject: subject,
       action_url: action_url,
       body_text: body_text,
