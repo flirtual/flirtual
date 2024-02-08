12a2f9f6922e6e628beacb857ec38b88b6f40508
Kyle Farwell <m@kfarwell.org>
2024-02-08 09:53:12 -0500

    disable android overscroll effect

--- apps/frontend/android/app/src/main/java/zone/homie/flirtual/pwa/MainActivity.java
+++ apps/frontend/android/app/src/main/java/zone/homie/flirtual/pwa/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -14,11 +15,17 @@ public class MainActivity extends BridgeActivity {
         createNotificationChannel();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webview = getBridge().getWebView();
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+    }
+
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
-                "flirtual", "Flirtual", NotificationManager.IMPORTANCE_HIGH
-            );
+                    "flirtual", "Flirtual", NotificationManager.IMPORTANCE_HIGH);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
