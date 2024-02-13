8d5ecf5e67902f22934d234a205d2d40acee2aab
Kyle Farwell <m@kfarwell.org>
2024-02-13 16:46:00 -0500

    increase likes page function timeout

--- apps/frontend/src/app/(authenticated)/(email-confirmed)/likes/page.tsx
+++ apps/frontend/src/app/(authenticated)/(email-confirmed)/likes/page.tsx
@@ -19,6 +19,8 @@ import { yearsAgo } from "~/date";
 import { HeartIcon } from "~/components/icons/gradient/heart";
 import { PeaceIcon } from "~/components/icons/gradient/peace";
 
+export const maxDuration = 60;
+
 export const metadata: Metadata = {
 	title: "Likes you"
 };
