a3e6978bad883e26a30faa3e0b29efe20a3b3451
Kyle Farwell <m@kfarwell.org>
2024-08-01 01:54:20 -0400

    fix: pfp keyboard shortcuts on direct profile

--- apps/frontend/src/components/profile/profile.tsx
+++ apps/frontend/src/components/profile/profile.tsx
@@ -72,7 +72,7 @@ export const Profile: FC<ProfileProps> = (props) => {
 			<div className="flex w-full flex-col overflow-hidden bg-transparent text-black-70 dark:text-white-20 desktop:rounded-[1.25rem] desktop:bg-white-20 desktop:shadow-brand-inset dark:desktop:bg-black-70">
 				<ProfileImageDisplay
 					images={user.profile.images}
-					current={id === "current-profile"}
+					current={id !== "next-profile"}
 				>
 					<div className="absolute bottom-0 flex w-full flex-col gap-2 p-8 text-white-10">
 						<div className="pointer-events-auto flex w-fit items-baseline gap-4 font-montserrat">
