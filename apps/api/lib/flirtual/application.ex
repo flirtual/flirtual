9c4c9e374a4316d224de3c592abfc2faf1348c3a
Kyle Farwell <m@kfarwell.org>
2024-08-13 18:04:59 -0400

    fix: format

--- apps/api/lib/flirtual/application.ex
+++ apps/api/lib/flirtual/application.ex
@@ -13,33 +13,40 @@ defmodule Flirtual.Application do
     Oban.Telemetry.attach_default_logger()
     Flirtual.ObanReporter.attach()
 
-    children = [
-      # Start the Cluster supervisor
-      {Cluster.Supervisor, [topologies, [name: Flirtual.ClusterSupervisor]]},
-      # Start the Ecto repository
-      Flirtual.Repo,
-      # Start Oban
-      {Oban, Application.fetch_env!(:flirtual, Oban)},
-      # Start Elasticsearch
-      Flirtual.Elasticsearch,
-      # Start the push notification dispatchers
-      if(Application.get_env(:flirtual, Flirtual.APNS)[:key] in [nil, ""],
-        do: Logger.warning("Flirtual.APNS not configured, excluding from supervision tree.") && nil,
-        else: Flirtual.APNS),
-      if(Application.get_env(:flirtual, Flirtual.FCM)[:key] in [nil, ""],
-        do: Logger.warning("Flirtual.FCM not configured, excluding from supervision tree.") && nil,
-        else: Flirtual.FCM),
-      # Start the Telemetry supervisor
-      FlirtualWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Flirtual.PubSub},
-      # Start the Endpoint (http/https)
-      FlirtualWeb.Endpoint,
-      {Finch, name: Swoosh.Finch},
-      # Start a worker by calling: Flirtual.Worker.start_link(arg)
-      # {Flirtual.Worker, arg}
-      Flirtual.AttributeOrderWorker
-    ] |> Enum.reject(&is_nil/1)
+    children =
+      [
+        # Start the Cluster supervisor
+        {Cluster.Supervisor, [topologies, [name: Flirtual.ClusterSupervisor]]},
+        # Start the Ecto repository
+        Flirtual.Repo,
+        # Start Oban
+        {Oban, Application.fetch_env!(:flirtual, Oban)},
+        # Start Elasticsearch
+        Flirtual.Elasticsearch,
+        # Start the push notification dispatchers
+        if(Application.get_env(:flirtual, Flirtual.APNS)[:key] in [nil, ""],
+          do:
+            Logger.warning("Flirtual.APNS not configured, excluding from supervision tree.") &&
+              nil,
+          else: Flirtual.APNS
+        ),
+        if(Application.get_env(:flirtual, Flirtual.FCM)[:key] in [nil, ""],
+          do:
+            Logger.warning("Flirtual.FCM not configured, excluding from supervision tree.") && nil,
+          else: Flirtual.FCM
+        ),
+        # Start the Telemetry supervisor
+        FlirtualWeb.Telemetry,
+        # Start the PubSub system
+        {Phoenix.PubSub, name: Flirtual.PubSub},
+        # Start the Endpoint (http/https)
+        FlirtualWeb.Endpoint,
+        {Finch, name: Swoosh.Finch},
+        # Start a worker by calling: Flirtual.Worker.start_link(arg)
+        # {Flirtual.Worker, arg}
+        Flirtual.AttributeOrderWorker
+      ]
+      |> Enum.reject(&is_nil/1)
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
