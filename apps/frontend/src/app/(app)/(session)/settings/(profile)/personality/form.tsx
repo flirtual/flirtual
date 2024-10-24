5477e01033c5f934886ad5cf8c05a95ebb7b1fe2
Kyle Farwell <m@kfarwell.org>
2024-10-24 18:48:34 -0400

    fix: personality update button text

--- apps/frontend/src/app/(app)/(session)/settings/(profile)/personality/form.tsx
+++ apps/frontend/src/app/(app)/(session)/settings/(profile)/personality/form.tsx
@@ -64,7 +64,7 @@ export const PersonalityForm: FC<{ personality: ProfilePersonality }> = ({
 							)}
 						</FormField>
 					))}
-					<FormButton />
+					<FormButton>Update</FormButton>
 				</>
 			)}
 		</Form>
