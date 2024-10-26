e3b4ef411357512c87d8bfaeaa0bf76fe6eb59b1
Aries <10256477+ariesclark@users.noreply.github.com>
2024-10-25 18:34:24 -0600

    wip: changes

--- apps/frontend/src/hooks/use-share.tsx
+++ apps/frontend/src/hooks/use-share.tsx
@@ -5,17 +5,14 @@ import useSWR from "swr";
 export function useShare() {
 	const { data: canShare } = useSWR(
 		"canShare",
-		async () => (await Share.canShare()).value,
+		async () => (await Share.canShare().catch(() => ({ value: false }))).value,
 		{ fallbackData: false }
 	);
 
 	return {
 		canShare,
-		share: useCallback(
-			async (options: ShareOptions): Promise<ShareResult> => {
-				return Share.share(options);
-			},
-			[canShare]
-		)
+		share: useCallback(async (options: ShareOptions): Promise<ShareResult> => {
+			return Share.share(options);
+		}, [])
 	};
 }
--- apps/frontend/src/hooks/use-tour.ts
+++ apps/frontend/src/hooks/use-tour.ts
@@ -116,109 +116,109 @@ export function useDefaultTour(enabled: boolean = true) {
 			({ next, back, cancel }) =>
 				enabled
 					? [
-						{
-							id: "introduction",
-							title: "Flirtual Tutorial",
-							text: `
+							{
+								id: "introduction",
+								title: "Flirtual Tutorial",
+								text: `
 					Take a quick tour with us! We can't wait to introduce you to some amazing people :)
 					`,
-							buttons: [
-								{
-									text: "Exit",
-									action: cancel
-								},
-								{
-									classes: "primary shadow-brand-1",
-									text: "Continue",
-									action: next
-								}
-							]
-						},
-						{
-							id: "like",
-							title: "Like their profile?",
-							text: "Press the <b>Like button</b>! If they like you back, you'll match.",
-							attachTo: { element: "#like-button", on: "top" },
-							modalOverlayOpeningRadius: 33
-						},
-						{
-							id: "friend",
-							title: "Like their profile?",
-							text: "Or press the <b>Homie button</b> if you want to be friends. You'll still match if it's mutual whether you Like or Homie each other.",
-							attachTo: { element: "#friend-button", on: "top" },
-							modalOverlayOpeningRadius: 33
-						},
-						{
-							id: "pass",
-							title: "Not interested?",
-							text: "Press the <b>Pass button</b> to move on to the next profile.",
-							attachTo: { element: "#pass-button", on: "top" },
-							modalOverlayOpeningRadius: 26
-						},
-						{
-							id: "undo",
-							title: "Changed your mind?",
-							text: "Press <b>Undo</b> to go back and see the last profile.",
-							attachTo: { element: "#undo-button", on: "top" },
-							modalOverlayOpeningRadius: 26
-						},
-						{
-							id: "conversations",
-							title: "Shoot your shot!",
-							text: "View your matches here. Message them and meet up in VR!",
-							attachTo: {
-								element: "#conversation-button",
-								on: "top"
+								buttons: [
+									{
+										text: "Exit",
+										action: cancel
+									},
+									{
+										classes: "primary shadow-brand-1",
+										text: "Continue",
+										action: next
+									}
+								]
 							},
-							modalOverlayOpeningRadius: 20,
-							modalOverlayOpeningPadding: 4
-						},
-						{
-							id: "browse-mode",
-							title: "Looking for something else?",
-							text: `
+							{
+								id: "like",
+								title: "Like their profile?",
+								text: "Press the <b>Like button</b>! If they like you back, you'll match.",
+								attachTo: { element: "#like-button", on: "top" },
+								modalOverlayOpeningRadius: 33
+							},
+							{
+								id: "friend",
+								title: "Like their profile?",
+								text: "Or press the <b>Homie button</b> if you want to be friends. You'll still match if it's mutual whether you Like or Homie each other.",
+								attachTo: { element: "#friend-button", on: "top" },
+								modalOverlayOpeningRadius: 33
+							},
+							{
+								id: "pass",
+								title: "Not interested?",
+								text: "Press the <b>Pass button</b> to move on to the next profile.",
+								attachTo: { element: "#pass-button", on: "top" },
+								modalOverlayOpeningRadius: 26
+							},
+							{
+								id: "undo",
+								title: "Changed your mind?",
+								text: "Press <b>Undo</b> to go back and see the last profile.",
+								attachTo: { element: "#undo-button", on: "top" },
+								modalOverlayOpeningRadius: 26
+							},
+							{
+								id: "conversations",
+								title: "Shoot your shot!",
+								text: "View your matches here. Message them and meet up in VR!",
+								attachTo: {
+									element: "#conversation-button",
+									on: "top"
+								},
+								modalOverlayOpeningRadius: 20,
+								modalOverlayOpeningPadding: 4
+							},
+							{
+								id: "browse-mode",
+								title: "Looking for something else?",
+								text: `
 					Switch between <b>Date Mode</b> and <b>Homie Mode</b> (without matchmaking filters) to see more profiles.<br/><br/>
 					Each day, you can browse up to <b>30 profiles</b> and Like or Homie up to 15 of them in each mode.`,
-							attachTo: {
-								element: "#browse-mode-switch",
-								on: "top"
+								attachTo: {
+									element: "#browse-mode-switch",
+									on: "top"
+								},
+								modalOverlayOpeningRadius: mobile ? 28 : 33
 							},
-							modalOverlayOpeningRadius: mobile ? 28 : 33
-						},
-						{
-							id: "profile-dropdown",
-							title: "Customize your experience!",
-							text: "Here you can <b>update your profile</b>, or subscribe to Premium to browse unlimited profiles and see who likes you before you match.",
-							attachTo: {
-								element: "#profile-dropdown-button",
-								on: "top"
+							{
+								id: "profile-dropdown",
+								title: "Customize your experience!",
+								text: "Here you can <b>update your profile</b>, or subscribe to Premium to browse unlimited profiles and see who likes you before you match.",
+								attachTo: {
+									element: "#profile-dropdown-button",
+									on: "top"
+								},
+								modalOverlayOpeningRadius: 20,
+								modalOverlayOpeningPadding: 4
 							},
-							modalOverlayOpeningRadius: 20,
-							modalOverlayOpeningPadding: 4
-						},
-						{
-							id: "conclusion",
-							title: "Thank you!",
-							text: `
+							{
+								id: "conclusion",
+								title: "Thank you!",
+								text: `
 					That concludes our tutorial! We hope you have a great time here, and remember to treat each other kindly.<br/><br/>
 					Don't forget to drink water and take breaks as needed!<br/><br/>
 					&lt;3<br/>
 					The Flirtual Team`,
-							modalOverlayOpeningRadius: 20,
-							modalOverlayOpeningPadding: 4,
-							buttons: [
-								{
-									text: "Back",
-									action: back
-								},
-								{
-									classes: "primary shadow-brand-1",
-									text: "Start matching",
-									action: next
-								}
-							]
-						}
-					]
+								modalOverlayOpeningRadius: 20,
+								modalOverlayOpeningPadding: 4,
+								buttons: [
+									{
+										text: "Back",
+										action: back
+									},
+									{
+										classes: "primary shadow-brand-1",
+										text: "Start matching",
+										action: next
+									}
+								]
+							}
+						]
 					: [],
 			[]
 		),
