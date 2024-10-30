c2f3e10d13dd9d13ca5d37528ad84c3832027e0f
Kyle Farwell <m@kfarwell.org>
2024-10-30 04:40:21 -0400

    no matches copy edit

--- apps/frontend/src/app/(app)/(session)/(onboarded)/matches/layout.tsx
+++ apps/frontend/src/app/(app)/(session)/(onboarded)/matches/layout.tsx
@@ -1,18 +1,20 @@
+import type { PropsWithChildren } from "react";
 import { unstable_serialize } from "swr";
 import { unstable_serialize as unstable_serialize_infinite } from "swr/infinite";
 
-import { fromEntries } from "~/utilities";
+import { Conversation } from "~/api/conversations";
+import { Matchmaking } from "~/api/matchmaking";
+import { User } from "~/api/user";
+import { ButtonLink } from "~/components/button";
+import { InlineLink } from "~/components/inline-link";
+import { ModelCard } from "~/components/model-card";
 import { SWRConfig } from "~/components/swr";
 import { getConversationsKey } from "~/hooks/use-conversations.shared";
-import { Conversation } from "~/api/conversations";
-import { User } from "~/api/user";
-import { Matchmaking } from "~/api/matchmaking";
-import { ButtonLink } from "~/components/button";
-import { urls } from "~/urls";
-import { ModelCard } from "~/components/model-card";
 import { userKey } from "~/swr";
+import { urls } from "~/urls";
+import { fromEntries } from "~/utilities";
 
-import type { PropsWithChildren } from "react";
+import { LikesYouButton } from "./likes-you-button";
 
 export default async function ConversationsLayout({
 	children
@@ -27,18 +29,31 @@ export default async function ConversationsLayout({
 			<ModelCard
 				branded
 				containerProps={{ className: "flex flex-col gap-4" }}
-				title="You're new"
+				title="Matches"
 			>
-				<p>You haven&apos;t matched with anyone yet.</p>
+				<h1 className="text-2xl font-semibold">
+					You haven&apos;t matched with anyone yet
+				</h1>
 				<p>
-					We prioritize showing you people that have liked or homied you, and
-					showing your profile to people you have liked or homied, so you can
-					match.
+					When someone likes or homies you back, it&apos;s a match! You&apos;ll be able to message them and meet up in VR.
 				</p>
-				<p>Until then, you can always:</p>
-				<ButtonLink href={urls.browse()} size="sm">
-					Browse profiles
-				</ButtonLink>
+				<p>
+					We prioritize showing you people who like or homie you, so you can match.
+				</p>
+				<details>
+					<summary className="text-pink opacity-75 transition-opacity hover:cursor-pointer hover:opacity-100">
+						Tips
+					</summary>
+					We&apos;ll show your profile to more people when you use Flirtual actively, like/homie people, and complete your profile details.
+					{" "}
+					<InlineLink href={urls.socials.discord}>Join our Discord</InlineLink>
+					{" "}
+					to share your profile and get feedback!
+				</details>
+				<div className="flex flex-col gap-2">
+					<ButtonLink href={urls.browse()} size="sm">Browse profiles</ButtonLink>
+					<LikesYouButton />
+				</div>
 			</ModelCard>
 		);
 
