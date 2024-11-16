fab40ac8208d504506871fbcad3f35ea416a453d
Aries <10256477+ariesclark@users.noreply.github.com>
2024-11-16 10:52:09 -0700

    fix?: stacked layout issues

--- apps/frontend/src/app/(app)/layout.tsx
+++ apps/frontend/src/app/(app)/layout.tsx
@@ -4,15 +4,9 @@ import { Footer } from "~/components/layout/footer";
 
 import { Navigation } from "./navigation";
 
-export default function AppLayout({
-	children,
-	banner
-}: PropsWithChildren<{
-	banner: ReactNode;
-}>) {
+export default function AppLayout({ children }: PropsWithChildren) {
 	return (
 		<div className="flex min-h-screen grow flex-col items-center bg-white-20 font-nunito text-black-80 vision:bg-transparent dark:bg-black-70 dark:text-white-20 desktop:flex-col desktop:bg-cream desktop:dark:bg-black-80">
-			{banner}
 			<Navigation />
 			<div
 				className="flex min-h-[calc(100svh-max(calc(env(safe-area-inset-bottom,0rem)+4.5rem),5rem))] w-full grow flex-col items-center desktop:p-8"
