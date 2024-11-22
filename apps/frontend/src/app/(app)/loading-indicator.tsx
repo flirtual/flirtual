d9bc19b67509128abbca8b090e31b0f545ffb5de
Kyle Farwell <m@kfarwell.org>
2024-11-22 18:18:50 -0500

    update loading indicator visionOS style

--- apps/frontend/src/app/(app)/loading-indicator.tsx
+++ apps/frontend/src/app/(app)/loading-indicator.tsx
@@ -11,7 +11,7 @@ export function LoadingIndicator() {
 			<Image
 				priority
 				alt={t("meta.name")}
-				className="hidden w-1/2 max-w-sm animate-pulse dark:block"
+				className="hidden w-1/2 max-w-sm animate-pulse vision:block dark:block"
 				height={1000}
 				src={urls.media("flirtual-white.svg", "files")}
 				width={3468}
@@ -19,7 +19,7 @@ export function LoadingIndicator() {
 			<Image
 				priority
 				alt={t("meta.name")}
-				className="block w-1/2 max-w-sm animate-pulse dark:hidden"
+				className="block w-1/2 max-w-sm animate-pulse vision:hidden dark:hidden"
 				height={1000}
 				src={urls.media("flirtual-black.svg", "files")}
 				width={3468}
