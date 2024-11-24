3a9ec97e848ce088c4769152335b80d0515bf3d3
Aries <me@ariesclark.com>
2024-11-24 12:53:23 -0700

    fix: Fallback data is required when using Suspense in SSR.

--- apps/frontend/src/app/(app)/(session)/(onboarded)/matches/layout.tsx
+++ apps/frontend/src/app/(app)/(session)/(onboarded)/matches/layout.tsx
@@ -24,39 +24,6 @@ export default async function ConversationsLayout({
 		Matchmaking.likesYou()
 	]);
 
-	if (conversations.length === 0)
-		return (
-			<ModelCard
-				branded
-				containerProps={{ className: "flex flex-col gap-4" }}
-				title="Matches"
-			>
-				<h1 className="text-2xl font-semibold">
-					You'll find your matches here
-				</h1>
-				<p>
-					When someone likes or homies you back, it&apos;s a match! You&apos;ll be able to message them and meet up in VR.
-				</p>
-				<p>
-					We prioritize showing you people who like or homie you, so you can match.
-				</p>
-				<details>
-					<summary className="text-pink opacity-75 transition-opacity hover:cursor-pointer hover:opacity-100">
-						Tips
-					</summary>
-					We&apos;ll show your profile to more people when you use Flirtual actively, like/homie people, and complete your profile details.
-					{" "}
-					<InlineLink href={urls.socials.discord}>Join our Discord</InlineLink>
-					{" "}
-					to share your profile and get feedback!
-				</details>
-				<div className="flex flex-col gap-2">
-					<ButtonLink href={urls.browse()} size="sm">Browse profiles</ButtonLink>
-					<LikesYouButton />
-				</div>
-			</ModelCard>
-		);
-
 	const userIds = conversations.map(({ userId }) => userId);
 	const userById = fromEntries((await User.getMany(userIds)).filter(Boolean).map((user) => [user.id, user]));
 
@@ -74,7 +41,39 @@ export default async function ConversationsLayout({
 				}
 			}}
 		>
-			{children}
+			{conversations.length === 0
+				? (
+						<ModelCard
+							branded
+							containerProps={{ className: "flex flex-col gap-4" }}
+							title="Matches"
+						>
+							<h1 className="text-2xl font-semibold">
+								You'll find your matches here
+							</h1>
+							<p>
+								When someone likes or homies you back, it&apos;s a match! You&apos;ll be able to message them and meet up in VR.
+							</p>
+							<p>
+								We prioritize showing you people who like or homie you, so you can match.
+							</p>
+							<details>
+								<summary className="text-pink opacity-75 transition-opacity hover:cursor-pointer hover:opacity-100">
+									Tips
+								</summary>
+								We&apos;ll show your profile to more people when you use Flirtual actively, like/homie people, and complete your profile details.
+								{" "}
+								<InlineLink href={urls.socials.discord}>Join our Discord</InlineLink>
+								{" "}
+								to share your profile and get feedback!
+							</details>
+							<div className="flex flex-col gap-2">
+								<ButtonLink href={urls.browse()} size="sm">Browse profiles</ButtonLink>
+								<LikesYouButton />
+							</div>
+						</ModelCard>
+					)
+				: children}
 		</SWRConfig>
 	);
 }
