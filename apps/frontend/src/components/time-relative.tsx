6b6b13dbe0ab7102aa4531de95b7275db0b44e29
Kyle Farwell <m@kfarwell.org>
2025-01-22 20:52:03 -0500

    fix: talkjs rerender, time relative

--- apps/frontend/src/components/time-relative.tsx
+++ apps/frontend/src/components/time-relative.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { type createFormatter, useFormatter } from "next-intl";
-import { type ComponentProps, type FC, useMemo } from "react";
+import { type ComponentProps, type FC, useCallback, useMemo, useState } from "react";
 
-import { RefreshInterval } from "./refresh-interval";
+import { useInterval } from "~/hooks/use-interval";
 
 type RelativeTimeFormatOptions = Exclude<
 	Parameters<ReturnType<typeof createFormatter>["relativeTime"]>[1],
@@ -17,18 +17,30 @@ interface TimeRelativeProps extends RelativeTimeFormatOptions {
 }
 
 export const TimeRelative: FC<TimeRelativeProps> = (props) => {
-	const { every = "1s", value, elementProps, ...options } = props;
+	const { value, every: _every, elementProps, ...options } = props;
 
 	const date = useMemo(() => new Date(value), [value]);
+	const [now, setNow] = useState(() => Date.now());
+
+	const difference = now - date.getTime();
+
+	let every = _every;
+	if (!every) {
+		every = "1s";
+		if (difference > 60 * 1000) every = "1m";
+		if (difference > 60 * 60 * 1000) every = "1h";
+	}
+
 	const formatter = useFormatter();
 
+	useInterval(
+		useCallback(() => setNow(Date.now()), []),
+		every
+	);
+
 	return (
-		<RefreshInterval every={every}>
-			{() => (
-				<span suppressHydrationWarning {...elementProps}>
-					{formatter.relativeTime(date, options)}
-				</span>
-			)}
-		</RefreshInterval>
+		<span suppressHydrationWarning {...elementProps}>
+			{formatter.relativeTime(date, { ...options, now })}
+		</span>
 	);
 };
