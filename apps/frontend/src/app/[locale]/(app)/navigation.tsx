3f530b0ffc774b59ca8cec93d5d5c590b2e21fa1
Aries <me@ariesclark.com>
2025-06-13 07:24:31 -0600

    fix: add device information display in preview mode

--- apps/frontend/src/app/[locale]/(app)/navigation.tsx
+++ apps/frontend/src/app/[locale]/(app)/navigation.tsx
@@ -8,6 +8,8 @@ import { HeartIcon } from "~/components/icons/gradient/heart";
 import { HomeIcon } from "~/components/icons/gradient/home";
 import { LoginIcon } from "~/components/icons/gradient/login";
 import { PeaceIcon } from "~/components/icons/gradient/peace";
+import { environment } from "~/const";
+import { device } from "~/hooks/use-device";
 import { useOptionalSession } from "~/hooks/use-session";
 import { urls } from "~/urls";
 
@@ -78,8 +80,23 @@ const NavigationContent = withSuspense(() => {
 });
 
 export function Navigation() {
+	const { deviceId, userAgent, native, versions: { commit, version } } = device;
+
 	return (
 		<header className="sticky bottom-0 z-50 order-last flex w-screen flex-col items-center justify-center bg-brand-gradient text-white-20 shadow-brand-1 vision:hidden desktop:bottom-auto desktop:top-0 desktop:order-none">
+			{environment === "preview" && (
+				<div className="pointer-events-none absolute bottom-20 flex max-w-lg flex-col items-center justify-center px-4 text-center text-xs opacity-75 desktop:bottom-[unset] desktop:top-24">
+					<span>{deviceId}</span>
+					<span>{userAgent}</span>
+					<span>{`${commit}`}</span>
+					{native && (
+						<span>
+							v
+							{version}
+						</span>
+					)}
+				</div>
+			)}
 			<div className="flex size-full items-center justify-evenly gap-4 px-5 pb-[max(calc(var(--safe-area-inset-bottom,0rem)-0.625rem),0.5rem)] pt-2 font-nunito text-white-20 desktop:w-auto desktop:pb-2">
 				<NavigationContent />
 			</div>
