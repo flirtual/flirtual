220697085d08cb8c234bfad9d342ec894e5effd6
Kyle Farwell <m@kfarwell.org>
2025-07-08 20:36:37 -0400

    update console banner

--- apps/frontend/src/instrumentation-client.ts
+++ apps/frontend/src/instrumentation-client.ts
@@ -3,16 +3,25 @@ import { App } from "@capacitor/app";
 import { urls } from "~/urls";
 
 import { applyDocumentMutations } from "./app/[locale]/lazy-layout";
+import { guessLocale } from "./i18n/routing";
 import {
 	preloadAll,
 	restoreQueries,
 	saveQueries
 } from "./query";
 
+const translations = {
+	en: () => import("~/../messages/en.json"),
+	ja: () => import("~/../messages/ja.json"),
+};
+
+const locale = guessLocale();
+const t = await translations[locale]();
+
 // eslint-disable-next-line no-console
 console.log(
-	`%cWant to contribute to Flirtual?`,
-	"padding: 1rem 2rem; background-image: linear-gradient(to right, #ff8975, #e9658b); color: white; white-space: pre; display: block; text-align: center; font-weight: bold; border-radius: .5rem",
+	`%c${t.console_message}`,
+	"padding: 0 0.5rem; background-image: linear-gradient(to right, #ff8975, #e9658b); color: white; white-space: pre; display: block; text-align: center; font-weight: bold; border-radius: .5rem",
 	urls.resources.developers
 );
 
