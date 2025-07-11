b3c39edc56cb880178618f30d5af391b8c40786e
Aries <me@ariesclark.com>
2025-07-11 17:00:13 -0600

    wip: changes

--- apps/frontend/src/app/[locale]/(app)/(authenticated)/(admin)/test/vrchat-browse/page.tsx
+++ apps/frontend/src/app/[locale]/(app)/(authenticated)/(admin)/test/vrchat-browse/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Cone, Dices, Flame, Gamepad2, Sprout } from "lucide-react";
-import type { ComponentProps, Dispatch, DispatchWithoutAction, FC, JSX } from "react";
+import type { InfiniteData } from "@tanstack/react-query";
+import { Cone, Dices, Ellipsis, Flame, Gamepad2, Sprout } from "lucide-react";
+import { type ComponentProps, type Dispatch, type DispatchWithoutAction, type FC, type JSX, use } from "react";
 import { useInView } from "react-intersection-observer";
 import { capitalize, uniqueBy } from "remeda";
 import { twMerge } from "tailwind-merge";
+import { withSuspense } from "with-suspense";
 
 import type { World, WorldCategory } from "~/api/vrchat";
 import { VRChat, worldCategories } from "~/api/vrchat";
@@ -41,11 +43,18 @@ const WorldItem: FC<{ world: World; onInvite?: DispatchWithoutAction }> = ({ wor
 						{world.authorName}
 					</span>
 				</div>
-				{onInvite && (
-					<Button className="h-fit py-1" onClick={onInvite}>
-						Invite
-					</Button>
-				)}
+				<div className="flex items-center gap-2">
+					{onInvite && (
+						<Button className="h-fit grow py-1" onClick={onInvite}>
+							Invite
+						</Button>
+					)}
+					<Button
+						className="aspect-square h-fit shrink-0 p-1"
+						Icon={Ellipsis}
+						kind="tertiary"
+					/>
+				</div>
 			</div>
 
 		</div>
@@ -54,18 +63,21 @@ const WorldItem: FC<{ world: World; onInvite?: DispatchWithoutAction }> = ({ wor
 
 const WorldItemSkeleton: FC<ComponentProps<"div">> = ({ className, ...props }) => {
 	return (
-		<div {...props} className={twMerge("flex w-56 shrink-0 snap-start snap-always flex-col gap-2 rounded-xl border bg-white-10 p-4", className)}>
-			<div className="aspect-[256/192] animate-pulse rounded-lg bg-gray-200" />
-			<div className="flex flex-col gap-0.5">
-				<span className="h-4 w-full animate-pulse rounded-sm bg-gray-200" />
-				<span className="h-4 w-3/4 animate-pulse rounded-sm bg-gray-200 opacity-75" />
+		<div {...props} className={twMerge("flex w-40 shrink-0 snap-start snap-always flex-col rounded-xl border bg-white-10 desktop:w-56", className)}>
+			<div className="aspect-[256/192] animate-pulse rounded-xl rounded-b-none bg-gray-200" />
+			<div className="flex flex-col gap-2 p-2 desktop:p-4">
+				<div className="flex flex-col gap-1">
+					<span className="h-6 w-full animate-pulse rounded-sm bg-gray-200" />
+					<span className="h-5 w-3/4 animate-pulse rounded-sm bg-gray-200 opacity-75" />
+				</div>
+				<span className="h-8 w-2/3 animate-pulse rounded-xl bg-gray-200" />
 			</div>
 		</div>
 	);
 };
 
-const WorldCategoryList: FC<{ category: WorldCategory }> = ({ category }) => {
-	const { data, isFetching, fetchNextPage } = useInfiniteQuery({
+const WorldCategory: FC<{ category: WorldCategory }> = ({ category }) => {
+	const { promise, fetchNextPage } = useInfiniteQuery({
 		queryKey: ["vrchat", "worlds", category],
 		queryFn: ({ pageParam: page }) => VRChat.getWorldsByCategory(category, page),
 		initialPageParam: 0,
@@ -76,17 +88,6 @@ const WorldCategoryList: FC<{ category: WorldCategory }> = ({ category }) => {
 
 	const Icon = worldCategoryIcons[category];
 
-	const [loadMoreReference] = useInView({
-		onChange: (inView) => {
-			console.log({ category, inView, isFetching });
-			if (!inView || isFetching) return;
-			fetchNextPage();
-		},
-	});
-
-	// We might receive duplicate worlds across pages, so we need to deduplicate them.
-	const worlds = uniqueBy(data?.pages.flat() ?? [], (world) => world.id);
-
 	return (
 		<div className="flex w-full flex-col gap-4">
 			<h2 className="flex items-center gap-2 font-bold">
@@ -95,14 +96,54 @@ const WorldCategoryList: FC<{ category: WorldCategory }> = ({ category }) => {
 				{capitalize(category)}
 			</h2>
 			<div className="flex w-full snap-x snap-proximity gap-2 overflow-x-auto overflow-y-visible scroll-smooth desktop:gap-4">
-				{worlds.map((world) => (
-					<WorldItem key={world.id} world={world} />
-				))}
-				<WorldItemSkeleton ref={loadMoreReference} />
+				<WorldCategoryContent
+					promise={promise}
+					onEndVisible={() => fetchNextPage()}
+				/>
 			</div>
 		</div>
 	);
 };
+
+const WorldCategoryContent = withSuspense<{
+	promise: Promise<InfiniteData<Array<World>, unknown>>;
+	onEndVisible?: DispatchWithoutAction;
+}>(({ promise, onEndVisible }) => {
+	const data = use(promise);
+
+	const [loadMoreReference] = useInView({
+		onChange: (inView) => {
+			if (!inView) return;
+			onEndVisible?.();
+		},
+	});
+
+	// We might receive duplicate worlds across pages, so we need to deduplicate them.
+	const worlds = uniqueBy(data?.pages.flat() ?? [], (world) => world.id);
+
+	return (
+		<>
+			{worlds.map((world) => (
+				<WorldItem
+					key={world.id}
+					world={world}
+					onInvite={() => {
+
+					}}
+				/>
+			))}
+			<WorldItemSkeleton ref={loadMoreReference} />
+		</>
+	);
+}, {
+	fallback:
+	<>
+		{Array.from({ length: 10 }).map((_, index) => (
+			// eslint-disable-next-line react/no-array-index-key
+			<WorldItemSkeleton key={index} />
+		))}
+	</>
+});
 
 export default function Test() {
 	return (
@@ -117,7 +158,7 @@ export default function Test() {
 				<DialogBody>
 					<div data-vaul-no-drag className="flex w-full flex-col gap-4">
 						{worldCategories.map((category) => (
-							<WorldCategoryList category={category} key={category} />
+							<WorldCategory category={category} key={category} />
 						))}
 					</div>
 				</DialogBody>
