08df2bc1a49e91dfb3b971f635a29fce07e0ceff
Kyle Farwell <m@kfarwell.org>
2025-08-27 18:21:46 -0400

    remove swr

--- apps/frontend/src/app/[locale]/(app)/(public)/confirm-email/confirm-token-form.tsx
+++ apps/frontend/src/app/[locale]/(app)/(public)/confirm-email/confirm-token-form.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import useMutation from "swr/mutation";
 
 import { User } from "~/api/user";
 import { Loading } from "~/components/loading";
 import { useToast } from "~/hooks/use-toast";
 import { useNavigate } from "~/i18n";
-import { invalidate, sessionKey } from "~/query";
+import { invalidate, sessionKey, useMutation } from "~/query";
 import { urls } from "~/urls";
 
 export const ConfirmTokenForm: React.FC<{ token: string }> = ({ token }) => {
@@ -14,28 +13,28 @@ export const ConfirmTokenForm: React.FC<{ token: string }> = ({ token }) => {
 	const navigate = useNavigate();
 	const { t } = useTranslation();
 
-	const { trigger } = useMutation(
-		"confirm-email",
-		(_, { arg: token }: { arg: string }) => User.confirmEmail(token),
-		{
-			onError: () => {
-				navigate(urls.confirmEmail());
+	const { mutateAsync } = useMutation({
+		mutationKey: sessionKey(),
+		mutationFn: async (token: string) => {
+			await User.confirmEmail(token);
+		},
+		onError: async () => {
+			toasts.add({
+				type: "error",
+				value: t("dizzy_fair_goat_propel"),
+				duration: "short"
+			});
 
-				toasts.add({
-					type: "error",
-					value: t("dizzy_fair_goat_propel"),
-					duration: "short"
-				});
-			},
-			onSuccess: async () => {
-				toasts.add(t("royal_home_leopard_tickle"));
+			await navigate(urls.confirmEmail());
+		},
+		onSuccess: async () => {
+			toasts.add(t("royal_home_leopard_tickle"));
 
-				await invalidate({ queryKey: sessionKey() });
-				navigate(urls.default);
-			}
+			await invalidate({ queryKey: sessionKey() });
+			await navigate(urls.default);
 		}
-	);
+	});
 
-	useEffect(() => void trigger(token), [trigger, token]);
+	useEffect(() => void mutateAsync(token), [mutateAsync, token]);
 	return <Loading className="absolute inset-0" />;
 };
