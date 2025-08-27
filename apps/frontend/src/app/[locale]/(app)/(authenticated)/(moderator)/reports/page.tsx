a0345ca236ffd43e017d440d5282cad978e775f7
Aries <me@ariesclark.com>
2025-08-27 09:09:09 -0600

    fix: page titles

--- apps/frontend/src/app/[locale]/(app)/(authenticated)/(moderator)/reports/page.tsx
+++ apps/frontend/src/app/[locale]/(app)/(authenticated)/(moderator)/reports/page.tsx
@@ -1,12 +1,21 @@
-// import { defaultLocale, i18n } from "~/i18n";
+import { defaultLocale, i18n } from "~/i18n";
 import { metaMerge, rootMeta } from "~/meta";
 
 import type { Route } from "./+types/page";
 import { ReportView } from "./report-view";
 
 export const meta: Route.MetaFunction = (options) => {
-	// const t = i18n.getFixedT(options.params.locale ?? defaultLocale);
-	return metaMerge([...rootMeta(options), { title: "Reports" }]);
+	const { params: { locale } } = options;
+	const t = i18n.getFixedT(locale ?? defaultLocale);
+
+	return metaMerge([
+		...rootMeta(options),
+		{
+			title: t("page_title", {
+				name: "Reports"
+			})
+		}
+	]);
 };
 
 export default function ReportPage() {
