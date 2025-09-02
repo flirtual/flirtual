9682cd3667ef3ea43e91f31d20b109c8918a49f0
Kyle Farwell <m@kfarwell.org>
2025-09-02 16:30:13 -0400

    fix: show tour only in date mode

--- apps/frontend/src/app/[locale]/(app)/(authenticated)/(onboarded)/discover/queue.tsx
+++ apps/frontend/src/app/[locale]/(app)/(authenticated)/(onboarded)/discover/queue.tsx
@@ -4,10 +4,21 @@ import type { FC } from "react";
 
 import type { ProspectKind } from "~/api/matchmaking";
 import { Profile } from "~/components/profile";
+import { useDevice } from "~/hooks/use-device";
 import { useQueue } from "~/hooks/use-queue";
+import { useSession } from "~/hooks/use-session";
+import { useDefaultTour } from "~/hooks/use-tour";
 
 import { ConfirmEmailError, FinishProfileError, OutOfProspects } from "./out-of-prospects";
 import { QueueActions } from "./queue-actions";
+
+function DefaultTour() {
+	const { user } = useSession();
+	const { vision } = useDevice();
+	useDefaultTour(!user.moderatorMessage && !vision);
+
+	return null;
+}
 
 export const Queue: FC<{ kind: ProspectKind }> = ({ kind }) => {
 	const { error, next: [current] } = useQueue(kind);
@@ -19,29 +30,29 @@ export const Queue: FC<{ kind: ProspectKind }> = ({ kind }) => {
 	if (error === "confirm_email")
 		return <ConfirmEmailError />;
 
+	if (!current) return <OutOfProspects mode={kind} />;
+
 	return (
 		<>
-			{current
-				? (
-						<Suspense>
-							<div className="relative max-w-full gap-4 overflow-hidden">
-								<AnimatePresence initial={false}>
-									<m.div
-										key={current}
-										animate={{ opacity: 1 }}
-										className="relative top-0 z-10"
-										exit={{ opacity: 0, position: "absolute" }}
-										initial={{ opacity: 0 }}
-									>
-										<Profile userId={current} />
-									</m.div>
-								</AnimatePresence>
-							</div>
-						</Suspense>
-					)
-				: <OutOfProspects mode={kind} />}
+			{kind === "love" && <DefaultTour />}
 
-			{/* <Suspense>
+			<div className="relative max-w-full gap-4 overflow-hidden">
+				<AnimatePresence initial={false}>
+					<m.div
+						key={current}
+						animate={{ opacity: 1 }}
+						className="relative top-0 z-10"
+						exit={{ opacity: 0, position: "absolute" }}
+						initial={{ opacity: 0 }}
+					>
+						<Profile userId={current} />
+					</m.div>
+				</AnimatePresence>
+			</div>
+
+			<QueueActions kind={kind} />
+
+			{/*
 				<div
 					className="relative top-0 z-10 grid grid-cols-3 gap-4"
 				>
@@ -49,8 +60,7 @@ export const Queue: FC<{ kind: ProspectKind }> = ({ kind }) => {
 					{current ? <Profile userId={current} /> : <div />}
 					{next ? <Profile userId={next} /> : <div />}
 				</div>
-			</Suspense> */}
-			<QueueActions kind={kind} />
+			*/}
 		</>
 	);
 };
