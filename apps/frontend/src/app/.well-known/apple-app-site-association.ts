cb1cc24e87cd9681eae3e73af8287e5f68cbbac0
Kyle Farwell <m@kfarwell.org>
2025-10-15 20:23:12 -0400

    make .well-known env vars optional

--- apps/frontend/src/app/.well-known/apple-app-site-association.ts
+++ apps/frontend/src/app/.well-known/apple-app-site-association.ts
@@ -1,1 +1,1 @@
-export const loader = () => JSON.parse(import.meta.env.VITE_APPLE_APP_SITE_ASSOCIATION_JSON);
+export const loader = () => JSON.parse(import.meta.env.VITE_APPLE_APP_SITE_ASSOCIATION_JSON || "{}");
--- apps/frontend/src/app/.well-known/apple-developer-merchantid-domain-association.ts
+++ apps/frontend/src/app/.well-known/apple-developer-merchantid-domain-association.ts
@@ -1,1 +1,1 @@
-export const loader = () => import.meta.env.VITE_APPLE_DEVELOPER_MERCHANTID_DOMAIN_ASSOCIATION;
+export const loader = () => import.meta.env.VITE_APPLE_DEVELOPER_MERCHANTID_DOMAIN_ASSOCIATION || "";
