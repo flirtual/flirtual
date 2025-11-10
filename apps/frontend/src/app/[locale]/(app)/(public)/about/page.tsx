52d4170208cf3e77ca94068624bce8d21fe068b3
Kyle Farwell <m@kfarwell.org>
2025-11-09 19:20:00 -0500

    update team list

--- apps/frontend/src/app/[locale]/(app)/(public)/about/page.tsx
+++ apps/frontend/src/app/[locale]/(app)/(public)/about/page.tsx
@@ -78,96 +78,79 @@ export default function AboutPage() {
 									{
 										name: "kfarwell",
 										role: teamRole.kfarwell,
-										avatar: "108161fa-6e0d-435c-a4fd-5bdd0909aec5",
-										url: urls.profile("DjkQHrZ3qjsBumHGBbEyS3")
+										avatar: "108161fa-6e0d-435c-a4fd-5bdd0909aec5"
 									},
 									{
 										name: "Tony",
 										role: teamRole.existony,
-										avatar: "faa26662-298a-47f3-838d-c36f40ca20f8",
-										url: urls.profile("e3ZfMyZLq2ouAPRoLZHKrU")
+										avatar: "faa26662-298a-47f3-838d-c36f40ca20f8"
 									},
 									{
 										name: "Aries",
 										role: teamRole.aries,
-										avatar: "957b0933-f010-447c-ab92-35e600367b70",
-										url: urls.profile("GumsFYN6GkXPMcAGog9pM4")
+										avatar: "957b0933-f010-447c-ab92-35e600367b70"
 									},
 									{
 										name: "Buramie",
 										role: teamRole.moderator,
-										avatar: "5a01f443-7270-4fdb-b903-73d909a6f0f6",
-										url: urls.profile("uQt9hi63LxYP6Cok6Znuz3")
+										avatar: "5a01f443-7270-4fdb-b903-73d909a6f0f6"
 									},
 									{
-										name: "FFHK",
+										name: "Cherry",
 										role: teamRole.moderator,
-										avatar: "47405223-1850-4116-a21a-4132fbed51ff",
-										url: urls.profile("fv2DHfXotnd26mS2yucwbX")
+										avatar: "ba650224-1ce1-4d5c-8e3e-9cd0638e520c"
 									},
 									{
-										name: "Kart²",
+										name: "Damned_United",
 										role: teamRole.moderator,
-										avatar: "18263ba2-f2e6-45b3-9e12-1cabbf3a1556",
-										url: urls.profile("mMFE4M3CvbrvL4dP2LywHT")
+										avatar: "1f8775ea-1f44-4e33-8cd7-388588723e5d"
 									},
 									{
-										name: "Lvcidu",
+										name: "Rem",
 										role: teamRole.moderator,
-										avatar: "49623cce-d03e-443a-822d-80412d28290d",
-										url: urls.profile("RiAEmdNcdbKYXHw5n6kepB")
+										avatar: "911150fc-4123-4ad3-ae9b-554a7709ad2b"
 									},
 									{
 										name: "Reploidsham",
 										role: teamRole.moderator,
-										avatar: "f93f2191-1dd4-4550-a348-b7634e3aff78",
-										url: urls.profile("ZxqVfAQWSmRH3r3RG6MkMf")
+										avatar: "f93f2191-1dd4-4550-a348-b7634e3aff78"
 									},
 									{
 										name: "Simon3373",
 										role: teamRole.moderator,
-										avatar: "4c30da22-1438-45c9-968e-a2a3a67e9cc7",
-										url: urls.profile("WVhKhLx89bDbUweT4BhKaW")
+										avatar: "4c30da22-1438-45c9-968e-a2a3a67e9cc7"
 									},
 									{
 										name: "Starh",
 										role: teamRole.moderator,
-										avatar: "57c46b77-862d-42a6-84b2-f97879bd6f23",
-										url: urls.profile("KKidURg3PuYzGGqErGAADC")
+										avatar: "57c46b77-862d-42a6-84b2-f97879bd6f23"
 									},
 									{
 										name: "The_Blarg",
 										role: teamRole.moderator,
-										avatar: "e3793c96-e6df-4520-9ead-d4fc4b1f2d9f",
-										url: urls.profile("CtCeB3m7jSDTaKTu2GF6HN")
+										avatar: "e3793c96-e6df-4520-9ead-d4fc4b1f2d9f"
 									},
 									{
 										name: "Zyp",
 										role: teamRole.moderator,
-										avatar: "78830b45-2d17-4caa-80eb-6ce728b991bf",
-										url: urls.profile("Am4uyyviqQfEy7F44XsJz6")
+										avatar: "78830b45-2d17-4caa-80eb-6ce728b991bf"
 									},
 									{
 										name: "Syrmor",
-										role: teamRole.syrmor,
-										avatar: "52dc4f3a-00a9-42d7-8a88-09bfe01bfc1f",
-										url: "https://www.youtube.com/c/syrmor"
+										role: teamRole.advisor,
+										avatar: "52dc4f3a-00a9-42d7-8a88-09bfe01bfc1f"
 									},
 									{
 										name: "Faxmashine",
 										role: teamRole.faxmashine,
 										avatar: "83977789-d464-49b5-b89e-955ffe8097c8",
-										url: "https://www.faxmashine.com/",
-										extra_url:
-										"https://vrchat.com/home/world/wrld_f5844d7c-dc4d-4ee7-bf3f-63c8e6be5539"
+										url: "https://www.faxmashine.com/"
 									},
 									{
 										name: "Juice",
 										role: teamRole.juice,
 										avatar: "c2ec6685-04ff-42e7-be66-c0a58e5afb6d",
-										url: "https://info.mdcr.tv/",
-										extra_url:
-										"https://vrchat.com/home/avatar/avtr_75b9f5a5-def3-4620-b547-0bf88677f449"
+										url: "https://info.mdcr.tv/"
 									}
 								]}
 							</TeamList>
--- apps/frontend/src/app/[locale]/(app)/(public)/about/team-member.tsx
+++ apps/frontend/src/app/[locale]/(app)/(public)/about/team-member.tsx
@@ -1,10 +1,8 @@
 import { ExternalLink } from "lucide-react";
 import { useTranslation } from "react-i18next";
-import { twMerge } from "tailwind-merge";
 
 import { Image } from "~/components/image";
 import { InlineLink } from "~/components/inline-link";
-import { useOptionalSession } from "~/hooks/use-session";
 import { urls } from "~/urls";
 
 export interface TeamMemberProps {
@@ -12,22 +10,15 @@ export interface TeamMemberProps {
 	role: string;
 	avatar: string;
 	url?: string;
-	extra_url?: string;
 }
 
 export const TeamMember: React.FC<TeamMemberProps> = (props) => {
-	const { name, role, avatar, url, extra_url } = props;
+	const { name, role, avatar, url } = props;
 
-	const session = useOptionalSession();
 	const { t } = useTranslation();
 
 	return (
-		<div
-			className={twMerge(
-				"flex items-center gap-4 text-inherit",
-				!url && "cursor-default"
-			)}
-		>
+		<div className="flex items-center gap-4 text-inherit">
 			<Image
 				alt={t("extra_moving_jackdaw_twist", { name })}
 				className="aspect-square w-12 shrink-0 rounded-full bg-black-70 object-cover shadow-brand-1"
@@ -36,19 +27,13 @@ export const TeamMember: React.FC<TeamMemberProps> = (props) => {
 				width="64"
 			/>
 			<div className="flex flex-col">
-				<InlineLink
-					className={twMerge(
-						"font-montserrat text-lg font-semibold text-pink",
-						!url && "cursor-default hocus:no-underline"
-					)}
-					href={(session && url) ?? null}
-				>
+				<span className="font-montserrat text-lg font-semibold">
 					{name}
-				</InlineLink>
+				</span>
 				<span>
 					{role}
-					{extra_url && (
-						<InlineLink className="text-pink" href={extra_url}>
+					{url && (
+						<InlineLink className="text-pink hocus:no-underline" href={url}>
 							{" "}
 							<ExternalLink className="inline size-4" />
 						</InlineLink>
