8fb7a737e53e64599f120a7dcfda5a92d85203e4
Kyle Farwell <m@kfarwell.org>
2025-11-11 22:59:39 -0500

    set SameSite=None in dev so cookies work in tidewave

--- apps/api/lib/flirtual_web/session.ex
+++ apps/api/lib/flirtual_web/session.ex
@@ -2,8 +2,10 @@ defmodule FlirtualWeb.Session do
   def init(opts), do: opts
 
   def call(conn, _opts) do
+    same_site = if Mix.env() == :dev, do: "None", else: "Lax"
+
     runtime_opts =
-      [store: :cookie, same_site: "Lax", max_age: 3_888_000, key: "session"]
+      [store: :cookie, same_site: same_site, max_age: 3_888_000, key: "session"]
       |> Keyword.put(:domain, Application.fetch_env!(:flirtual, :root_origin) |> Map.get(:host))
       |> Keyword.put(:signing_salt, Application.fetch_env!(:flirtual, :session_signing_salt))
 
