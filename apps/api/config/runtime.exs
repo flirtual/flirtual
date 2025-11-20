be3e061e6455f7e7c715ee18e6cd317217ed3092
Kyle Farwell <m@kfarwell.org>
2025-11-19 20:10:42 -0500

    fix: typo

--- apps/api/config/runtime.exs
+++ apps/api/config/runtime.exs
@@ -23,7 +23,7 @@ config :flirtual, Flirtual.ObanWorkers,
   enabled_cron_tasks:
     if(config_env() == :prod,
       do: [:like_digest, :update_disposable, :prune_sessions, :prune_banned],
-      else: [:like_digest, :update_disposable, :prune_sessions, :update_disposable]
+      else: [:like_digest, :update_disposable, :prune_sessions]
     ),
   email_rate_limit: System.fetch_env!("EMAIL_RATE_LIMIT") |> String.to_integer()
 
