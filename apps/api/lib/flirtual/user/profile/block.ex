18ed65b1a583a4347db022154930417aa117951b
Kyle Farwell <m@kfarwell.org>
2025-12-04 04:12:34 -0500

    increment passes when blocking prospect

--- apps/api/lib/flirtual/user/profile/block.ex
+++ apps/api/lib/flirtual/user/profile/block.ex
@@ -5,6 +5,7 @@ defmodule Flirtual.User.Profile.Block do
   import Ecto.Changeset
 
   alias Flirtual.{ObanWorkers, Repo, User}
+  alias Flirtual.User.Profile
   alias Flirtual.User.Profile.{Block, LikesAndPasses, Prospect}
 
   @derive {Jason.Encoder, only: [:profile_id, :target_id, :created_at]}
@@ -21,6 +22,12 @@ defmodule Flirtual.User.Profile.Block do
   end
 
   def create(user_id: user_id, target_id: target_id) do
+    prospect_kinds =
+      Prospect
+      |> where(profile_id: ^user_id, target_id: ^target_id, completed: false)
+      |> select([p], p.kind)
+      |> Repo.all()
+
     Repo.transaction(fn repo ->
       with {:ok, item} <-
              %Block{}
@@ -29,6 +36,7 @@ defmodule Flirtual.User.Profile.Block do
              |> Repo.insert(),
            {:ok, _} <- LikesAndPasses.delete_all(profile_id: user_id, target_id: target_id),
            {:ok, _} <- Prospect.delete_all(profile_id: user_id, target_id: target_id),
+           :ok <- increment_passes(user_id, prospect_kinds),
            {:ok, _} <- ObanWorkers.update_user([user_id, target_id], [:elasticsearch, :talkjs]) do
         item
       else
@@ -36,6 +44,26 @@ defmodule Flirtual.User.Profile.Block do
         reason -> Repo.rollback(reason)
       end
     end)
+  end
+
+  defp increment_passes(_, []), do: :ok
+
+  defp increment_passes(user_id, kinds) do
+    queue_increments =
+      Enum.map(kinds, fn
+        :love -> {:queue_love_passes, 1}
+        :friend -> {:queue_friend_passes, 1}
+      end)
+
+    Profile
+    |> where(user_id: ^user_id)
+    |> Repo.update_all(inc: queue_increments)
+
+    User
+    |> where(id: ^user_id)
+    |> Repo.update_all(inc: [passes_count: 1])
+
+    :ok
   end
 
   def get(user: %User{id: user_id}, target: %User{id: target_id}) do
