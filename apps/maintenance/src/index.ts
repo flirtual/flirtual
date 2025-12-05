36e2794b61e56128437fecf3d938e5d8a96ab65e
Kyle Farwell <m@kfarwell.org>
2025-12-04 21:47:12 -0500

    update maintenance, route

--- apps/maintenance/src/index.ts
+++ apps/maintenance/src/index.ts
@@ -3,7 +3,11 @@ import { env } from "cloudflare:workers"
 const hourInSeconds = 60 * 60;
 
 export default {
-  async fetch(request) {
+  async fetch(request: Request) {
+		const cookie = request.headers.get("cookie");
+		if (cookie && cookie.includes(`flirtual-maintenance=${env.TOKEN}`))
+			return fetch(request);
+
 		const response = await env.ASSETS.fetch(request);
 
 		const headers = new Headers(response.headers);
