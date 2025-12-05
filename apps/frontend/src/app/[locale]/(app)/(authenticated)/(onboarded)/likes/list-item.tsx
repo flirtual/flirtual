10fb3e1973e7ec7029f6fd8ae05d15e256214e9c
Kyle Farwell <m@kfarwell.org>
2025-12-04 22:36:32 -0500

    add likes list item suspense

--- apps/frontend/src/app/[locale]/(app)/(authenticated)/(onboarded)/likes/list-item.tsx
+++ apps/frontend/src/app/[locale]/(app)/(authenticated)/(onboarded)/likes/list-item.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { withSuspense } from "with-suspense";
 
 import type { LikeAndPassItem } from "~/api/matchmaking";
 import { HeartIcon } from "~/components/icons/gradient/heart";
@@ -13,7 +14,7 @@ import { yearsAgo } from "~/date";
 import { useUser } from "~/hooks/use-user";
 import { urls } from "~/urls";
 
-export const LikeListItem: FC<LikeAndPassItem> = ({ kind, profileId: userId }) => {
+export const LikeListItem: FC<LikeAndPassItem> = withSuspense(({ kind, profileId: userId }) => {
 	const user = useUser(userId);
 	const { t } = useTranslation();
 
@@ -54,4 +55,19 @@ export const LikeListItem: FC<LikeAndPassItem> = ({ kind, profileId: userId }) =
 			<Icon className="h-8 shrink-0 pr-4 desktop:mr-2" />
 		</Link>
 	);
-};
+}, {
+	fallback: (
+		<div className="flex items-center gap-4 bg-white-10 p-4 vision:bg-white-10/10 dark:bg-black-80 desktop:rounded-xl desktop:shadow-brand-1">
+			<div className="size-16 shrink-0 animate-pulse rounded-lg bg-black-90/10 bg-brand-gradient dark:bg-white-10/20" />
+			<div className="flex w-full grow flex-col gap-2">
+				<span className="h-6 w-32 animate-pulse rounded-xl bg-black-90/10 text-xl font-semibold dark:bg-white-10/20 desktop:text-2xl" />
+				<div className="flex w-full gap-2">
+					<div className="h-8 w-10 animate-pulse rounded-xl bg-black-80/10 dark:bg-white-10/10" />
+					<div className="h-8 w-20 animate-pulse rounded-xl bg-black-80/10 dark:bg-white-10/10" />
+					<div className="h-8 w-11 animate-pulse rounded-xl bg-black-80/10 dark:bg-white-10/10" />
+				</div>
+			</div>
+			<div className="size-10 shrink-0 animate-pulse rounded-3xl bg-black-90/10 pr-4 dark:bg-white-10/20 desktop:mr-5" />
+		</div>
+	)
+});
