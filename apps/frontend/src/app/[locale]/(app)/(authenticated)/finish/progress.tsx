f09c5266d56964e986e02cdad1b490cf73f8c55b
Kyle Farwell <m@kfarwell.org>
2026-07-16 16:51:28 -0700

    disable progress links until progress/1 is complete

--- apps/frontend/src/app/[locale]/(app)/(authenticated)/finish/progress.tsx
+++ apps/frontend/src/app/[locale]/(app)/(authenticated)/finish/progress.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 
 import { InlineLink } from "~/components/inline-link";
+import { useOptionalSession } from "~/hooks/use-session";
 import { urls } from "~/urls";
 import type { FinishPage } from "~/urls";
 
@@ -9,16 +10,18 @@ const ProgressLink: React.FC<{
 	page: FinishPage;
 	name: string;
 	current: FinishPage;
-}> = ({ page, name, current }) => {
+	disabled?: boolean;
+}> = ({ page, name, current, disabled = false }) => {
 	const active = current >= page;
 
 	return (
 		<InlineLink
 			className={twMerge(
 				"z-10 hidden h-fit justify-center px-3 hocus:no-underline desktop:flex",
-				active ? "text-white-10" : "text-black-70 dark:text-white-10"
+				active ? "text-white-10" : "text-black-70 dark:text-white-10",
+				disabled && "opacity-50"
 			)}
-			href={urls.finish(page)}
+			href={disabled ? null : urls.finish(page)}
 		>
 			{name}
 		</InlineLink>
@@ -27,6 +30,13 @@ const ProgressLink: React.FC<{
 
 export const FinishProgress: React.FC<{ page: FinishPage }> = ({ page }) => {
 	const { t } = useTranslation();
+	const session = useOptionalSession();
+
+	const profile = session?.user.profile;
+	const locked
+		= !profile?.displayName
+			|| !profile.biography
+			|| profile.images.length === 0;
 
 	return (
 		<div className="fixed inset-x-0 bottom-[max(calc(var(--safe-area-inset-bottom,0rem)+4.5rem),5.25rem)] isolate z-10 flex h-7 w-full px-4 vision:bottom-3 vision:mx-4 vision:w-[calc(100%-2rem)] desktop:relative desktop:inset-0 desktop:mb-8 desktop:h-9 desktop:max-w-2xl">
@@ -48,10 +58,10 @@ export const FinishProgress: React.FC<{ page: FinishPage }> = ({ page }) => {
 			</div>
 			<div className="grid h-6 w-full grid-cols-3 items-center justify-center rounded-full bg-white-30 shadow-brand-1 vision:bg-white-30/70 dark:bg-black-60 desktop:h-9 desktop:max-w-2xl desktop:grid-cols-5">
 				<ProgressLink name={t("bio_pics")} current={page} page={1} />
-				<ProgressLink name={t("details")} current={page} page={2} />
-				<ProgressLink name={t("interests")} current={page} page={3} />
-				<ProgressLink name={t("personality")} current={page} page={4} />
-				<ProgressLink name={t("connections")} current={page} page={5} />
+				<ProgressLink name={t("details")} current={page} disabled={locked} page={2} />
+				<ProgressLink name={t("interests")} current={page} disabled={locked} page={3} />
+				<ProgressLink name={t("personality")} current={page} disabled={locked} page={4} />
+				<ProgressLink name={t("connections")} current={page} disabled={locked} page={5} />
 			</div>
 		</div>
 	);
