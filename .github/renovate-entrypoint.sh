cd2999a65458f0b8d74ea13dba4721f2de05f7b3
Kyle Farwell <m@kfarwell.org>
2026-08-14 16:34:28 -0700

    fix: renovate erlang versions

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# renovate: datasource=github-releases depName=erlang/otp extractVersion=^OTP-(?<version>.*)$
-install-tool erlang 27.3.3
+# renovate: datasource=github-releases depName=containerbase/erlang-prebuild versioning=semver-coerced
+install-tool erlang 27.3.3.0
 # renovate: datasource=github-releases depName=elixir-lang/elixir extractVersion=^v(?<version>.*)$
 install-tool elixir 1.18.3
 # renovate: datasource=node-version depName=node
