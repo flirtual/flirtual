3c466ee62a9b52a3a25873484b008aa23e2c2720
Kyle Farwell <m@kfarwell.org>
2026-08-14 15:43:17 -0700

    require elixir 1.18.3

--- apps/api/mix.exs
+++ apps/api/mix.exs
@@ -5,7 +5,7 @@ defmodule Flirtual.MixProject do
     [
       app: :flirtual,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
